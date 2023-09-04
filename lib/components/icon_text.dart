2d1feeb55b202094c88b387489872d33a9a9d0d2
Pascal JK <pascual.santos80@gmail.com>
2023-09-04 14:59:01 +0200

    - working on icon text file content.

--- lib/components/icon_text.dart
+++ lib/components/icon_text.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
 
+import 'text/small.dart';
+
 class IconText extends StatelessWidget {
-  const IconText({super.key});
+  const IconText({
+    super.key,
+    required this.icon,
+    required this.text,
+    required this.iconColor,
+  });
+  final IconData icon;
+  final String text;
+  final Color iconColor;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Row(
+      children: [
+        Icon(icon, color: iconColor),
+        const SizedBox(width: 5),
+        SmallText(text: text),
+      ],
+    );
   }
 }
