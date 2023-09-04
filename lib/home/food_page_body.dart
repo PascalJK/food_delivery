50c2846b44ef931e8c28c479eacb0c33f405be1a
Pascal JK <pascual.santos80@gmail.com>
2023-09-04 15:44:10 +0200

    - working on transform animation affects.

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -13,6 +13,25 @@ class FoodPageBody extends StatefulWidget {
 
 class _FoodPageBodyState extends State<FoodPageBody> {
   final pageController = PageController(viewportFraction: .85);
+  var _currentPageValue = 0.0;
+  var _scaleFactor = .8;
+  var _height = 220;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController.addListener(() {
+      setState(() {
+        _currentPageValue = pageController.page!;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    pageController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,78 +48,106 @@ class _FoodPageBodyState extends State<FoodPageBody> {
   }
 
   Widget _buildPageItem(int index) {
-    return Stack(
-      children: [
-        Container(
-          height: 220,
-          margin: const EdgeInsets.symmetric(horizontal: 8),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(30),
-            color: index.isEven ? const Color(0xff69c5df) : const Color(0xff9294cc),
-            image: const DecorationImage(
-              image: AssetImage('assets/image/food0.png'),
-              fit: BoxFit.cover,
-            ),
-          ),
-        ),
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: Container(
-            height: 120,
-            margin: const EdgeInsets.only(right: 20, left: 20, bottom: 30),
+    var matrix = Matrix4.identity();
+    // Current Item (inFocus)
+    if (index == _currentPageValue.floor()) {
+      var currentScale = 1 - (_currentPageValue - index) * (1 - _scaleFactor);
+      var currentTrans = _height * (1 - currentScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currentScale, 1)..setTranslationRaw(0, currentTrans, 0);
+    }
+    // Right Item
+    else if (index == _currentPageValue.floor() + 1) {
+      var currentScale = _scaleFactor + (_currentPageValue - index + 1) * (1 - _scaleFactor);
+      var currentTrans = _height * (1 - currentScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currentScale, 1)..setTranslationRaw(0, currentTrans, 0);
+    }
+    // Left Item
+    else if (index == _currentPageValue.floor() - 1) {
+      var currentScale = 1 - (_currentPageValue - index) * (1 - _scaleFactor);
+      var currentTrans = _height * (1 - currentScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currentScale, 1)..setTranslationRaw(0, currentTrans, 0);
+    }
+    // Apply to other unfocused items
+    else {
+      var currentTrans = _height * (1 - _scaleFactor) / 2;
+      matrix = Matrix4.diagonal3Values(1, _scaleFactor, 1)..setTranslationRaw(0, currentTrans, 0);
+    }
+
+    return Transform(
+      transform: matrix,
+      child: Stack(
+        children: [
+          Container(
+            height: 220,
+            margin: const EdgeInsets.symmetric(horizontal: 8),
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              color: Colors.white,
-            ),
-            child: Container(
-              padding: const EdgeInsets.only(top: 15, left: 15, right: 15),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const BigText(text: 'Bitter Orange Margarine'),
-                  const SizedBox(height: 10),
-                  Row(
-                    children: [
-                      Wrap(
-                        children: List.generate(
-                          5,
-                          (index) => Icon(Icons.star, color: AppColors.mainColor, size: 15),
-                        ),
-                      ),
-                      const SizedBox(width: 10),
-                      const SmallText(text: '4.5'),
-                      const VerticalDivider(width: 10, thickness: 5, color: Colors.black),
-                      const Icon(Icons.comment_outlined, size: 15),
-                      const SizedBox(width: 1),
-                      const SmallText(text: '123')
-                    ],
-                  ),
-                  const SizedBox(height: 20),
-                  Row(
-                    children: [
-                      IconText(
-                        icon: Icons.circle_sharp,
-                        text: 'Normal',
-                        iconColor: AppColors.iconColor1,
-                      ),
-                      IconText(
-                        icon: Icons.location_on,
-                        text: '1.7km',
-                        iconColor: AppColors.mainColor,
-                      ),
-                      IconText(
-                        icon: Icons.access_time_rounded,
-                        text: '32min',
-                        iconColor: AppColors.iconColor2,
-                      ),
-                    ],
-                  )
-                ],
+              borderRadius: BorderRadius.circular(30),
+              color: index.isEven ? const Color(0xff69c5df) : const Color(0xff9294cc),
+              image: const DecorationImage(
+                image: AssetImage('assets/image/food0.png'),
+                fit: BoxFit.cover,
               ),
             ),
           ),
-        ),
-      ],
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 120,
+              margin: const EdgeInsets.only(right: 20, left: 20, bottom: 30),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: Colors.white,
+              ),
+              child: Container(
+                padding: const EdgeInsets.only(top: 15, left: 15, right: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const BigText(text: 'Bitter Orange Margarine'),
+                    const SizedBox(height: 10),
+                    Row(
+                      children: [
+                        Wrap(
+                          children: List.generate(
+                            5,
+                            (index) => Icon(Icons.star, color: AppColors.mainColor, size: 15),
+                          ),
+                        ),
+                        const SizedBox(width: 10),
+                        const SmallText(text: '4.5'),
+                        const VerticalDivider(width: 10, thickness: 5, color: Colors.black),
+                        const Icon(Icons.comment_outlined, size: 15),
+                        const SizedBox(width: 1),
+                        const SmallText(text: '123'),
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                    Row(
+                      children: [
+                        IconText(
+                          icon: Icons.circle_sharp,
+                          text: 'Normal',
+                          iconColor: AppColors.iconColor1,
+                        ),
+                        IconText(
+                          icon: Icons.location_on,
+                          text: '1.7km',
+                          iconColor: AppColors.mainColor,
+                        ),
+                        IconText(
+                          icon: Icons.access_time_rounded,
+                          text: '32min',
+                          iconColor: AppColors.iconColor2,
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
