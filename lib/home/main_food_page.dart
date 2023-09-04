b38760462097b6a9fcec12d4a673987e2fdc9ce7
Pascal JK <pascual.santos80@gmail.com>
2023-09-04 13:15:46 +0200

    - working on file dummy ui.

--- lib/home/main_food_page.dart
+++ lib/home/main_food_page.dart
@@ -10,6 +10,25 @@ class MainFoodPage extends StatefulWidget {
 class _MainFoodPageState extends State<MainFoodPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      child: Container(
+        child: Row(
+          children: [
+            Column(
+              children: [
+                Text('Country'),
+                Text('City'),
+              ],
+            ),
+            Container(
+              width: 45,
+              height: 45,
+              decoration:
+                  BoxDecoration(borderRadius: BorderRadius.circular(15), color: Colors.blue),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
