c72dc66096909b19241456e4be0f18bcfe9b53cf
Pascal JK <pascual.santos80@gmail.com>
2023-09-04 14:47:04 +0200

    - added icon text file.

---
+++ lib/components/icon_text.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class IconText extends StatelessWidget {
+  const IconText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
