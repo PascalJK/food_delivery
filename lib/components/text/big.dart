e40d35f6ec15bfeeef372321bf8bb265298f4fec
Pascal JK <pascual.santos80@gmail.com>
2023-09-04 13:39:40 +0200

    - added new resuable text file.

---
+++ lib/components/text/big.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class BigText extends StatelessWidget {
+  const BigText({
+    super.key,
+    required this.text,
+    this.color = const Color(0xFF332d2b),
+    this.size = 20,
+    this.overflow = TextOverflow.ellipsis,
+  });
+  final String text;
+  final Color? color;
+  final double size;
+  final TextOverflow overflow;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      overflow: overflow,
+      style: TextStyle(
+        color: color,
+        fontFamily: 'Roboto',
+        fontSize: size,
+        fontWeight: FontWeight.w400,
+      ),
+    );
+  }
+}
