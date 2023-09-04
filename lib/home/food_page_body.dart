00e67bf847d5145c61296811026a2bb7893b04f9
Pascal JK <pascual.santos80@gmail.com>
2023-09-04 14:59:57 +0200

    - working on food page body ui.

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/components/icon_text.dart';
+import 'package:food_delivery/components/text/big.dart';
+import 'package:food_delivery/components/text/small.dart';
+import 'package:food_delivery/utils/colors.dart';
 
 class FoodPageBody extends StatefulWidget {
   const FoodPageBody({super.key});
@@ -8,11 +12,14 @@ class FoodPageBody extends StatefulWidget {
 }
 
 class _FoodPageBodyState extends State<FoodPageBody> {
+  final pageController = PageController(viewportFraction: .85);
+
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 320,
       child: PageView.builder(
+        controller: pageController,
         itemCount: 5,
         itemBuilder: (context, index) {
           return _buildPageItem(index);
@@ -26,7 +33,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
       children: [
         Container(
           height: 220,
-          margin: const EdgeInsets.symmetric(horizontal: 5),
+          margin: const EdgeInsets.symmetric(horizontal: 8),
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(30),
             color: index.isEven ? const Color(0xff69c5df) : const Color(0xff9294cc),
@@ -39,11 +46,57 @@ class _FoodPageBodyState extends State<FoodPageBody> {
         Align(
           alignment: Alignment.bottomCenter,
           child: Container(
-            height: 140,
-            margin: const EdgeInsets.only(right: 40, left: 40, bottom: 10),
+            height: 120,
+            margin: const EdgeInsets.only(right: 20, left: 20, bottom: 30),
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30),
+              borderRadius: BorderRadius.circular(15),
               color: Colors.white,
+            ),
+            child: Container(
+              padding: const EdgeInsets.only(top: 15, left: 15, right: 15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const BigText(text: 'Bitter Orange Margarine'),
+                  const SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Wrap(
+                        children: List.generate(
+                          5,
+                          (index) => Icon(Icons.star, color: AppColors.mainColor, size: 15),
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      const SmallText(text: '4.5'),
+                      const VerticalDivider(width: 10, thickness: 5, color: Colors.black),
+                      const Icon(Icons.comment_outlined, size: 15),
+                      const SizedBox(width: 1),
+                      const SmallText(text: '123')
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    children: [
+                      IconText(
+                        icon: Icons.circle_sharp,
+                        text: 'Normal',
+                        iconColor: AppColors.iconColor1,
+                      ),
+                      IconText(
+                        icon: Icons.location_on,
+                        text: '1.7km',
+                        iconColor: AppColors.mainColor,
+                      ),
+                      IconText(
+                        icon: Icons.access_time_rounded,
+                        text: '32min',
+                        iconColor: AppColors.iconColor2,
+                      ),
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ),
