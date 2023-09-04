bfd4216cd808259fafff66f1f0c7441decc16cea
Pascal JK <pascual.santos80@gmail.com>
2023-09-04 14:59:40 +0200

    - working on main food page ui.

--- lib/home/main_food_page.dart
+++ lib/home/main_food_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/home/food_page_body.dart';
+import 'package:food_delivery/utils/colors.dart';
+
+import '../components/text/big.dart';
+import '../components/text/small.dart';
 
 class MainFoodPage extends StatefulWidget {
   const MainFoodPage({super.key});
@@ -11,25 +16,47 @@ class _MainFoodPageState extends State<MainFoodPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Container(
-          child: Row(
-            children: [
-              Column(
+      body: Column(
+        children: [
+          Container(
+            child: Container(
+              margin: const EdgeInsets.only(top: 45, bottom: 15),
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text('Country'),
-                  Text('City'),
+                  Column(
+                    children: [
+                      BigText(
+                        text: 'India Bangala',
+                        color: AppColors.mainColor,
+                      ),
+                      const Row(
+                        children: [
+                          SmallText(
+                            text: 'Suniel City',
+                            color: Colors.black54,
+                          ),
+                          Icon(Icons.arrow_drop_down_rounded),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Container(
+                    width: 45,
+                    height: 45,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: AppColors.mainColor,
+                    ),
+                    child: const Icon(Icons.search, color: Colors.white),
+                  )
                 ],
               ),
-              Container(
-                width: 45,
-                height: 45,
-                decoration:
-                    BoxDecoration(borderRadius: BorderRadius.circular(15), color: Colors.blue),
-              )
-            ],
+            ),
           ),
-        ),
+          const FoodPageBody(),
+        ],
       ),
     );
   }
