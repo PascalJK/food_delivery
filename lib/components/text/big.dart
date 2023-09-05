6a9b30fed0d8af7c6876d0ac624122b9bc823e20
Pascal JK <pascual.santos80@gmail.com>
2023-09-05 22:17:34 +0200

    - updated size value.

--- lib/components/text/big.dart
+++ lib/components/text/big.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/utils/dimensions.dart';
 
 class BigText extends StatelessWidget {
   const BigText({
     super.key,
     required this.text,
     this.color = const Color(0xFF332d2b),
-    this.size = 20,
+    this.size = 0,
     this.overflow = TextOverflow.ellipsis,
   });
   final String text;
@@ -22,7 +23,7 @@ class BigText extends StatelessWidget {
       style: TextStyle(
         color: color,
         fontFamily: 'Roboto',
-        fontSize: size,
+        fontSize: size == 0 ? Dimensions.font20 : size,
         fontWeight: FontWeight.w400,
       ),
     );
