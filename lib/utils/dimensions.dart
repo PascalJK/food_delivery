72db27fa6c6a823680709e384ee93cbecfbfc034
Pascal JK <pascual.santos80@gmail.com>
2023-09-05 22:39:40 +0200

    - worked on dimension values.

--- lib/utils/dimensions.dart
+++ lib/utils/dimensions.dart
@@ -9,9 +9,27 @@ class Dimensions {
   static double pageViewContainer = screenHeight / 3.84;
   static double pageViewTextContainer = screenHeight / 7.03;
 
+// dynamic height padding + margin
   static double height10 = screenHeight / 84.4;
   static double height15 = screenHeight / 56.27;
   static double height20 = screenHeight / 42.2;
+  static double height45 = screenHeight / 18.76;
+
+// dynamic width padding + margin
+  static double width10 = screenWidth / 84.4;
+  static double width15 = screenWidth / 56.27;
+  static double width20 = screenHeight / 42.2;
+  static double width30 = screenWidth / 84.4;
+
+  static double size45 = screenHeight / 18.76;
+
+// radius
+  static double radius15 = screenHeight / 56.27;
+  static double radius20 = screenHeight / 42.2;
+  static double radius30 = screenHeight / 28.13;
+
+  // icon size
+  static double iconSize24 = screenHeight / 35.17;
 
   static double font20 = screenHeight / 42.2;
 }
