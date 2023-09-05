ea70c505e4946caa6ede42be37a3a84cf298d8ac
Pascal JK <pascual.santos80@gmail.com>
2023-09-05 21:57:13 +0200

    - working on file properties.

--- lib/utils/dimentions.dart
+++ lib/utils/dimentions.dart
@@ -3,7 +3,9 @@ import 'package:get/get.dart';
 class Dimentions {
   static double screenHeight = Get.context!.height;
   static double screenWidth = Get.context!.width;
-  
-  static double pageViewContainer = screenHeight/3.84;
-  static double pageViewTextContainer = screenHeight/7.03;
+
+// Equation == ScreenHeight / Desired Height
+  static double pageView = screenHeight / 2.64;
+  static double pageViewContainer = screenHeight / 3.84;
+  static double pageViewTextContainer = screenHeight / 7.03;
 }
