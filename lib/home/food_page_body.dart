f77ae43ce209f706c9b0ddc3add1e52f93f5128f
Pascal JK <pascual.santos80@gmail.com>
2023-09-05 22:16:57 +0200

    - working on hard coded values.

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -4,7 +4,7 @@ import 'package:food_delivery/components/icon_text.dart';
 import 'package:food_delivery/components/text/big.dart';
 import 'package:food_delivery/components/text/small.dart';
 import 'package:food_delivery/utils/colors.dart';
-import 'package:food_delivery/utils/dimentions.dart';
+import 'package:food_delivery/utils/dimensions.dart';
 
 class FoodPageBody extends StatefulWidget {
   const FoodPageBody({super.key});
@@ -41,7 +41,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
       children: [
         // PageView
         Container(
-          height: Dimentions.pageView,
+          height: Dimensions.pageView,
           child: PageView.builder(
             controller: pageController,
             itemCount: 5,
@@ -98,7 +98,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
       child: Stack(
         children: [
           Container(
-            height: Dimentions.pageViewContainer,
+            height: Dimensions.pageViewContainer,
             margin: const EdgeInsets.symmetric(horizontal: 8),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(30),
@@ -112,7 +112,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
           Align(
             alignment: Alignment.bottomCenter,
             child: Container(
-              height: Dimentions.pageViewTextContainer,
+              height: Dimensions.pageViewTextContainer,
               margin: const EdgeInsets.only(right: 20, left: 20, bottom: 30),
               decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(15),
@@ -128,7 +128,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     const BigText(text: 'Bitter Orange Margarine'),
-                    const SizedBox(height: 10),
+                    SizedBox(height: Dimensions.height10),
                     Row(
                       children: [
                         Wrap(
@@ -137,7 +137,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
                             (index) => Icon(Icons.star, color: AppColors.mainColor, size: 15),
                           ),
                         ),
-                        const SizedBox(width: 10),
+                        SizedBox(width: Dimensions.height10),
                         const SmallText(text: '4.5'),
                         const VerticalDivider(width: 10, thickness: 5, color: Colors.black),
                         const Icon(Icons.comment_outlined, size: 15),
@@ -145,7 +145,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
                         const SmallText(text: '123'),
                       ],
                     ),
-                    const SizedBox(height: 20),
+                    SizedBox(height: Dimensions.height20),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
