78323f78daef72cfd315eb391fa923832271fe21
Pascal JK <pascual.santos80@gmail.com>
2023-09-06 20:53:53 +0200

    - working on details content.

--- lib/pages/food/popular_food_detail.dart
+++ lib/pages/food/popular_food_detail.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/components/app_column.dart';
 import 'package:food_delivery/components/app_icon.dart';
+import 'package:food_delivery/components/text/big.dart';
 import 'package:food_delivery/utils/dimensions.dart';
 
 class PopularFoodDetail extends StatelessWidget {
@@ -10,30 +12,62 @@ class PopularFoodDetail extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
+          // Image
           Positioned(
             left: 0,
             right: 0,
             child: Container(
               height: Dimensions.popularFoodImgSize,
               width: double.maxFinite,
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                fit: BoxFit.cover,
-                image: AssetImage('assets/image/food0.png'),
-              )),
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage('assets/image/food0.png'),
+                ),
+              ),
             ),
           ),
+          // Buttons
           Positioned(
-              top: Dimensions.height45,
-              left: Dimensions.width20,
-              right: Dimensions.width20,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            top: Dimensions.height45,
+            left: Dimensions.width20,
+            right: Dimensions.width20,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                AppIcon(icon: Icons.arrow_back),
+                AppIcon(icon: Icons.shopping_cart_outlined),
+              ],
+            ),
+          ),
+          // Details
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            top: Dimensions.popularFoodImgSize - 15,
+            child: Container(
+              padding: EdgeInsets.only(
+                left: Dimensions.width20,
+                right: Dimensions.width20,
+                top: Dimensions.width20,
+              ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.vertical(top: Radius.circular(Dimensions.radius20)),
+                color: Colors.white,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  AppIcon(icon: Icons.arrow_back),
-                  AppIcon(icon: Icons.shopping_cart_outlined),
+                  AppColumn(
+                    size: Dimensions.font26,
+                  ),
+                  SizedBox(height: Dimensions.height10),
+                  const BigText(text: 'Introduce'),
                 ],
-              ))
+              ),
+            ),
+          )
         ],
       ),
     );
