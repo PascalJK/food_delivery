c7539fc9d7a37f568e94510cfe1b937544912eaa
Pascal JK <pascual.santos80@gmail.com>
2023-09-06 22:04:46 +0200

    - converted to stateful class.

--- lib/components/text/expandable_text.dart
+++ lib/components/text/expandable_text.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class ExpandableText extends StatelessWidget {
+class ExpandableText extends StatefulWidget {
   const ExpandableText({super.key});
 
+  @override
+  State<ExpandableText> createState() => _ExpandableTextState();
+}
+
+class _ExpandableTextState extends State<ExpandableText> {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
