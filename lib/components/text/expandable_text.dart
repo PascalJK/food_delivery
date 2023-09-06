0fbb468488632a75ebabae288910523d96a58d81
Pascal JK <pascual.santos80@gmail.com>
2023-09-06 22:04:03 +0200

    - created expandable text file.

---
+++ lib/components/text/expandable_text.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ExpandableText extends StatelessWidget {
+  const ExpandableText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
