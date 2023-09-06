d2a660457fb8e27014013528db6ba5ee616eeccb
Pascal JK <pascual.santos80@gmail.com>
2023-09-06 19:49:29 +0200

    - added new file.

---
+++ lib/components/app_icon.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class AppIcon extends StatelessWidget {
+  const AppIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
