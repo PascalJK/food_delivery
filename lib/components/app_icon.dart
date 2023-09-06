6b98f8c43f5d17377a6408ff1c5b47edbbad0aed
Pascal JK <pascual.santos80@gmail.com>
2023-09-06 19:57:41 +0200

    - working on interface content.

--- lib/components/app_icon.dart
+++ lib/components/app_icon.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/widgets.dart';
 
 class AppIcon extends StatelessWidget {
-  const AppIcon({super.key});
+  final IconData icon;
+  final Color bgColor;
+  final Color iconColor;
+  final double size;
+  const AppIcon({
+    super.key,
+    required this.icon,
+    this.bgColor = const Color(0xfffcf4e4),
+    this.iconColor = const Color(0xff756d54),
+    this.size = 40,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      height: size,
+      width: size,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(size / 2),
+        color: bgColor,
+      ),
+      child: Icon(
+        icon,
+        color: iconColor,
+        size: 16,
+      ),
+    );
   }
 }
