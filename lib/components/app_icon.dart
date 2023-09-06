e73f52d09a76f120d5626376f64f6c9243c32846
Pascal JK <pascual.santos80@gmail.com>
2023-09-06 20:10:10 +0200

    - code improvement.

--- lib/components/app_icon.dart
+++ lib/components/app_icon.dart
@@ -1,31 +1,32 @@
 import 'package:flutter/widgets.dart';
+import 'package:food_delivery/utils/dimensions.dart';
 
 class AppIcon extends StatelessWidget {
   final IconData icon;
-  final Color bgColor;
-  final Color iconColor;
-  final double size;
+  final Color? bgColor;
+  final Color? iconColor;
+  final double? size;
   const AppIcon({
     super.key,
     required this.icon,
-    this.bgColor = const Color(0xfffcf4e4),
-    this.iconColor = const Color(0xff756d54),
-    this.size = 40,
+    this.bgColor,
+    this.iconColor,
+    this.size,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: size,
-      width: size,
+      height: size ?? Dimensions.height40,
+      width: size ?? Dimensions.height40,
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(size / 2),
-        color: bgColor,
+        borderRadius: BorderRadius.circular(size ?? Dimensions.height40 / 2),
+        color: bgColor ?? const Color(0xfffcf4e4),
       ),
       child: Icon(
         icon,
-        color: iconColor,
-        size: 16,
+        color: iconColor ?? const Color(0xff756d54),
+        size: Dimensions.iconSize16,
       ),
     );
   }
