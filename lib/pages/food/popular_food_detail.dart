2eb625a225f300a41e87ce04bb6bd63a88b465e6
Pascal JK <pascual.santos80@gmail.com>
2023-09-07 14:20:28 +0200

    - code refactor.

--- lib/pages/food/popular_food_detail.dart
+++ lib/pages/food/popular_food_detail.dart
@@ -6,15 +6,10 @@ import 'package:food_delivery/components/text/expandable_text.dart';
 import 'package:food_delivery/utils/colors.dart';
 import 'package:food_delivery/utils/dimensions.dart';
 
+import '../../utils/text.dart';
+
 class PopularFoodDetail extends StatelessWidget {
   const PopularFoodDetail({super.key});
-
-  static String tListingInfoDetailTxt =
-      '- History: This Angus bull comes from a lineage of high-quality breeding, known for producing excellent beef cattle.\n'
-      '- Temperament: He has a calm and docile temperament, making him easy to handle and work with.\n'
-      '- Diet: He has been raised on a grass-fed diet with added grain supplements for optimal growth and health.\n'
-      '- Health Status: He has been regularly vaccinated and is in excellent health condition.\n'
-      '- Markings: He has a distinctive white spot on his forehead, adding to his unique appearance.';
 
   @override
   Widget build(BuildContext context) {
@@ -74,7 +69,7 @@ class PopularFoodDetail extends StatelessWidget {
                   SizedBox(height: Dimensions.height10),
                   const BigText(text: 'Introduce'),
                   SizedBox(height: Dimensions.height15),
-                  ExpandableText(text: tListingInfoDetailTxt)
+                  const ExpandableText(text: tListingInfoDetailTxt)
                 ],
               ),
             ),
