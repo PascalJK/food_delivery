dd6b52f9a91eb81cb55b8f91cfa9b89df3599af3
Pascal JK <pascual.santos80@gmail.com>
2023-09-07 14:20:08 +0200

    - added new const file.

---
+++ lib/utils/text.dart
@@ -0,0 +1,7 @@
+
+  const String tListingInfoDetailTxt =
+      '- History: This Angus bull comes from a lineage of high-quality breeding, known for producing excellent beef cattle.\n'
+      '- Temperament: He has a calm and docile temperament, making him easy to handle and work with.\n'
+      '- Diet: He has been raised on a grass-fed diet with added grain supplements for optimal growth and health.\n'
+      '- Health Status: He has been regularly vaccinated and is in excellent health condition.\n'
+      '- Markings: He has a distinctive white spot on his forehead, adding to his unique appearance.';
