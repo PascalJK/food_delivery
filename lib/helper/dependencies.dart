ec7bf358b3871f05b421c4a39e1f6fe90f3ca9fd
Pascal JK <pascual.santos80@gmail.com>
2023-09-07 22:54:09 +0200

    - working on init logic.

--- lib/helper/dependencies.dart
+++ lib/helper/dependencies.dart
@@ -1,3 +1,15 @@
-Future init()async{
-  
+import 'package:food_delivery/controllers/popular_product_controller.dart';
+import 'package:food_delivery/data/api_client.dart';
+import 'package:food_delivery/data/popular_product_repo.dart';
+import 'package:get/get.dart';
+
+Future init() async {
+  //api client
+  Get.lazyPut(() => ApiClient(appBaseUrl: 'https://www.google.com'));
+
+  // repos
+  Get.lazyPut(() => PopularProductRepo(apiClient: Get.find<ApiClient>()));
+
+  // controllers
+  Get.lazyPut(() => PopularProductController(popularProductRepo: Get.find<PopularProductRepo>()));
 }
