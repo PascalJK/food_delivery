c7c4258d2dff513fe4bf76dad4c897c9c46161d6
Pascal JK <pascual.santos80@gmail.com>
2023-09-07 14:56:28 +0200

    - working on page interface.

--- lib/pages/food/recommended_food_detail.dart
+++ lib/pages/food/recommended_food_detail.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/components/text/big.dart';
+import 'package:food_delivery/utils/colors.dart';
+import 'package:food_delivery/utils/dimensions.dart';
+import 'package:food_delivery/utils/text.dart';
+
+import '../../components/app_icon.dart';
+import '../../components/text/expandable_text.dart';
 
 class RecommendedFoodDetail extends StatelessWidget {
   const RecommendedFoodDetail({super.key});
@@ -9,13 +16,52 @@ class RecommendedFoodDetail extends StatelessWidget {
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
+            toolbarHeight: 70,
+            expandedHeight: 300,
+            pinned: true,
+            automaticallyImplyLeading: false,
+            backgroundColor: AppColors.yellowColor,
+            title: const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                AppIcon(icon: Icons.arrow_back),
+                AppIcon(icon: Icons.shopping_cart_outlined),
+              ],
+            ),
+            bottom: PreferredSize(
+              preferredSize: const Size.fromHeight(20),
+              child: Container(
+                width: double.maxFinite,
+                padding: const EdgeInsets.only(top: 5, bottom: 10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(Dimensions.radius20),
+                  ),
+                ),
+                child: Center(child: BigText(text: 'Chinese Side', size: Dimensions.font26)),
+              ),
+            ),
             flexibleSpace: FlexibleSpaceBar(
               background: Image.asset(
                 'assets/image/food0.png',
+                fit: BoxFit.cover,
                 width: double.maxFinite,
               ),
             ),
           ),
+          SliverToBoxAdapter(
+            child: Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: Dimensions.width20),
+                  child: const ExpandableText(
+                    text: '$tListingInfoDetailTxt...\n[Repeat]\n$tListingInfoDetailTxt',
+                  ),
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
