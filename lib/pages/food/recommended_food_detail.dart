fabbbf2a74bb1613d6b1a4fe2cab82ab3b8a61b0
Pascal JK <pascual.santos80@gmail.com>
2023-09-07 13:57:10 +0200

    - added new file recommended food detail

---
+++ lib/pages/food/recommended_food_detail.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RecommendedFoodDetail extends StatelessWidget {
+  const RecommendedFoodDetail({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
