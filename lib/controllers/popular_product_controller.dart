cdbfaa9d0ddba193cd872e21e5a4658280ba869f
Pascal JK <pascual.santos80@gmail.com>
2023-09-08 00:32:31 +0200

    - adding returned items to list.

--- lib/controllers/popular_product_controller.dart
+++ lib/controllers/popular_product_controller.dart
@@ -1,4 +1,5 @@
 import 'package:food_delivery/data/popular_product_repo.dart';
+import 'package:food_delivery/models/product_model.dart';
 import 'package:get/get.dart';
 
 class PopularProductController extends GetxController {
@@ -8,10 +9,17 @@ class PopularProductController extends GetxController {
   List<dynamic> _popularProductList = [];
   List<dynamic> get getPopularProductList => _popularProductList;
 
+  // @override
+  // Future<void> onInit() async {
+  //   super.onInit();
+  //   await loadPopularProductList();
+  // }
+
   Future loadPopularProductList() async {
     var response = await popularProductRepo.getPopularProductList();
     if (response.statusCode == 200) {
       _popularProductList = [];
+      _popularProductList.addAll(Product.fromJson(response.body).getProducts);
       update();
     } else {
       //      throw Exception("Error");
