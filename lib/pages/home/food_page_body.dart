1e1873eca2ba8772e4f815fffe512b6de3cdb3d1
Pascal JK <pascual.santos80@gmail.com>
2023-09-08 22:21:54 +0200

    - code clean up.

--- lib/pages/home/food_page_body.dart
+++ lib/pages/home/food_page_body.dart
@@ -6,7 +6,6 @@ import 'package:food_delivery/components/text/big.dart';
 import 'package:food_delivery/components/text/small.dart';
 import 'package:food_delivery/controllers/popular_product_controller.dart';
 import 'package:food_delivery/controllers/recommended_product_controller.dart';
-import 'package:food_delivery/pages/food/popular_food_detail.dart';
 import 'package:food_delivery/routes/route_helper.dart';
 import 'package:food_delivery/utils/app_constants.dart';
 import 'package:food_delivery/utils/colors.dart';
