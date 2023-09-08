1ee66fe231949d804b780e7d2ad91c938cb3f80d
Pascal JK <pascual.santos80@gmail.com>
2023-09-08 20:46:12 +0200

    - network handshake exception fix.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,23 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:food_delivery/pages/home/main_food_page.dart';
 import 'package:get/get.dart';
 import 'helper/dependencies.dart' as dep;
 
+// Added this code to fix the HandshakeException: Handshake error in client (OS Error: CERTIFICATE_VERIFY_FAILED)
+// Occurs when trying to load Network Image.
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port) => true;
+  }
+}
+
 Future<void> main() async {
+  // Network HandshakeEX fix
+  HttpOverrides.global = MyHttpOverrides();
   WidgetsFlutterBinding.ensureInitialized();
   await dep.init();
   runApp(const MainApp());
