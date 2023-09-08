9e7cbe40458a2db27e7e51d13d727ca3a07e4323
Pascal JK <pascual.santos80@gmail.com>
2023-09-08 19:54:52 +0200

    - code refactor

--- lib/helper/dependencies.dart
+++ lib/helper/dependencies.dart
@@ -1,11 +1,12 @@
 import 'package:food_delivery/controllers/popular_product_controller.dart';
 import 'package:food_delivery/data/api_client.dart';
 import 'package:food_delivery/data/popular_product_repo.dart';
+import 'package:food_delivery/utils/app_constants.dart';
 import 'package:get/get.dart';
 
 Future init() async {
   //api client
-  Get.lazyPut(() => ApiClient(appBaseUrl: 'https://mvs.bslmeiyu.com/'));
+  Get.lazyPut(() => ApiClient(appBaseUrl: AppConstants.BASE_URL));
 
   // repos
   Get.lazyPut(() => PopularProductRepo(apiClient: Get.find<ApiClient>()));
