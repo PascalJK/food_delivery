e0d6067440e39ab3ae6152a3a9a64b9b09fbb0a1
Pascal JK <pascual.santos80@gmail.com>
2023-09-08 19:56:51 +0200

    - added token property

--- lib/utils/app_constants.dart
+++ lib/utils/app_constants.dart
@@ -4,6 +4,8 @@ class AppConstants {
   static const String APP_NAME = 'Jokies Foodies';
   static const int APP_Version = 1;
 
+  static const String TOKEN = 'DBtonken';
+
   static const String BASE_URL = 'https://mvs.bslmeiyu.com';
   static const String POPULAR_PRODUCT_URI = '/api/v1/products/popular';
   static const String RECOMMENDED_PRODUCT_URI = '/api/v1/products/recommended';
