59e0bf424fd37026d629f8f9f949e4544d746d1c
Pascal JK <pascual.santos80@gmail.com>
2023-09-09 20:22:49 +0200

    - code refactor.

--- lib/pages/cart/cart_page.dart
+++ lib/pages/cart/cart_page.dart
@@ -53,89 +53,90 @@ class CartPage extends StatelessWidget {
           ),
           // list view
           Positioned(
-              top: Dimensions.height20 * 5,
-              left: Dimensions.width20,
-              right: Dimensions.width20,
-              bottom: 0,
-              child: Container(
-                margin: EdgeInsets.only(top: Dimensions.height15),
-                child: MediaQuery.removePadding(
-                  context: context,
-                  removeTop: true,
-                  child: GetBuilder<CartController>(builder: (c) {
-                    return ListView.builder(
-                      itemCount: c.getCartList.length,
-                      itemBuilder: (context, index) {
-                        final item = c.getCartList[index];
-                        return Container(
-                          height: 100,
-                          width: double.maxFinite,
-                          margin: const EdgeInsets.only(bottom: 8),
-                          child: Row(
-                            children: [
-                              Container(
-                                height: Dimensions.height20 * 5,
-                                width: Dimensions.height20 * 5,
-                                margin: EdgeInsets.only(bottom: Dimensions.height10),
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(Dimensions.radius20),
-                                  color: Colors.white,
-                                  image: DecorationImage(
-                                    image: NetworkImage(AppConstants.UPLOAD_URL + item.img!),
-                                    fit: BoxFit.cover,
-                                  ),
+            top: Dimensions.height20 * 5,
+            left: Dimensions.width20,
+            right: Dimensions.width20,
+            bottom: 0,
+            child: Container(
+              margin: EdgeInsets.only(top: Dimensions.height15),
+              child: MediaQuery.removePadding(
+                context: context,
+                removeTop: true,
+                child: GetBuilder<CartController>(builder: (c) {
+                  return ListView.builder(
+                    itemCount: c.getCartList.length,
+                    itemBuilder: (context, index) {
+                      final item = c.getCartList[index];
+                      return Container(
+                        height: 100,
+                        width: double.maxFinite,
+                        margin: const EdgeInsets.only(bottom: 8),
+                        child: Row(
+                          children: [
+                            Container(
+                              height: Dimensions.height20 * 5,
+                              width: Dimensions.height20 * 5,
+                              margin: EdgeInsets.only(bottom: Dimensions.height10),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(Dimensions.radius20),
+                                color: Colors.white,
+                                image: DecorationImage(
+                                  image: NetworkImage(AppConstants.UPLOAD_URL + item.img!),
+                                  fit: BoxFit.cover,
                                 ),
                               ),
-                              SizedBox(width: Dimensions.width10),
-                              Expanded(
-                                  child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                children: [
-                                  BigText(text: item.name!),
-                                  SmallText(text: 'text'),
-                                  Row(
-                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      BigText(text: '\$${item.price!}'),
-                                      Container(
-                                        padding: EdgeInsets.symmetric(
-                                          horizontal: Dimensions.height10,
-                                          vertical: Dimensions.height10,
-                                        ),
-                                        decoration: BoxDecoration(
-                                          borderRadius: BorderRadius.circular(Dimensions.radius20),
-                                          color: Colors.white,
-                                        ),
-                                        child: Row(
-                                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                          children: [
-                                            GestureDetector(
-                                              // onTap: () => c.setQuantity(false),
-                                              child: Icon(Icons.remove, color: AppColors.signColor),
-                                            ),
-                                            SizedBox(width: Dimensions.width10 / 2),
-                                            BigText(text: '${item.quantity}'),
-                                            SizedBox(width: Dimensions.width10 / 2),
-                                            GestureDetector(
-                                              // onTap: () => c.setQuantity(true),
-                                              child: Icon(Icons.add, color: AppColors.signColor),
-                                            ),
-                                          ],
-                                        ),
+                            ),
+                            SizedBox(width: Dimensions.width10),
+                            Expanded(
+                                child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                BigText(text: item.name!),
+                                SmallText(text: 'text'),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    BigText(text: '\$${item.price!}'),
+                                    Container(
+                                      padding: EdgeInsets.symmetric(
+                                        horizontal: Dimensions.height10,
+                                        vertical: Dimensions.height10,
                                       ),
-                                    ],
-                                  )
-                                ],
-                              ))
-                            ],
-                          ),
-                        );
-                      },
-                    );
-                  }),
-                ),
-              ))
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(Dimensions.radius20),
+                                        color: Colors.white,
+                                      ),
+                                      child: Row(
+                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          GestureDetector(
+                                            // onTap: () => c.setQuantity(false),
+                                            child: Icon(Icons.remove, color: AppColors.signColor),
+                                          ),
+                                          SizedBox(width: Dimensions.width10 / 2),
+                                          BigText(text: '${item.quantity}'),
+                                          SizedBox(width: Dimensions.width10 / 2),
+                                          GestureDetector(
+                                            // onTap: () => c.setQuantity(true),
+                                            child: Icon(Icons.add, color: AppColors.signColor),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                )
+                              ],
+                            ))
+                          ],
+                        ),
+                      );
+                    },
+                  );
+                }),
+              ),
+            ),
+          ),
         ],
       ),
     );
