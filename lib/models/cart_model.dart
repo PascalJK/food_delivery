6d18667c70e20f48125b9b6d512fad0a20a3f671
Pascal JK <pascual.santos80@gmail.com>
2023-09-09 14:06:10 +0200

    - added model properties

--- lib/models/cart_model.dart
+++ lib/models/cart_model.dart
@@ -1,3 +1,28 @@
 class CartModel {
-  
+  int? id;
+  String? name;
+  int? price;
+  String? img;
+  int? quantity;
+  bool? isExist;
+  String? time;
+  CartModel({
+    this.id,
+    this.name,
+    this.price,
+    this.img,
+    this.quantity,
+    this.isExist,
+    this.time,
+  });
+
+  CartModel.fromMap(Map<String, dynamic> map) {
+    id = map['id'];
+    name = map['name'];
+    price = map['price'];
+    img = map['img'];
+    quantity = map['quantity'];
+    isExist = map['isExist'];
+    time = map['time'];
+  }
 }
