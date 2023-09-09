ae5c2c26e8cfb1697d01b2f4b207ba3b2ccef872
Pascal JK <pascual.santos80@gmail.com>
2023-09-09 15:09:17 +0200

    - generated copywith method.

--- lib/models/cart_model.dart
+++ lib/models/cart_model.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 class CartModel {
   int? id;
   String? name;
@@ -24,5 +25,25 @@ class CartModel {
     quantity = map['quantity'];
     isExist = map['isExist'];
     time = map['time'];
+  }
+
+  CartModel copyWith({
+    int? id,
+    String? name,
+    int? price,
+    String? img,
+    int? quantity,
+    bool? isExist,
+    String? time,
+  }) {
+    return CartModel(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      price: price ?? this.price,
+      img: img ?? this.img,
+      quantity: quantity ?? this.quantity,
+      isExist: isExist ?? this.isExist,
+      time: time ?? this.time,
+    );
   }
 }
