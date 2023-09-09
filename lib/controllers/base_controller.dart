9d08852e4d958aecbcee54c1a60991ffd26c4758
Pascal JK <pascual.santos80@gmail.com>
2023-09-09 17:40:29 +0200

    - moved reusable popular product controller logic to base controller.

--- lib/controllers/base_controller.dart
+++ lib/controllers/base_controller.dart
@@ -1,5 +1,48 @@
 import 'package:get/get.dart';
 
+import '../models/product_model.dart';
+import 'cart_controller.dart';
+
 class BaseController extends GetxController {
+  final CartController _cartController = Get.find<CartController>();
+
+  int _cartItems = 0;
+  int _quantity = 0;
+
+  int get getCartItems => _cartItems + _quantity;
+  int get getTotalCartItemsQty => _cartController.getTotalQuantities();
+
+  String get getQuantity => '$_quantity';
   
+  void initProduct(ProductModel p) {
+    _cartItems = 0;
+    _quantity = _cartController.getQuantity(p);
+  }
+
+  void setQuantity(bool isIncrement) {
+    if (isIncrement) {
+      _quantity = checkQuantity(_quantity + 1);
+    } else {
+      _quantity = checkQuantity(_quantity - 1);
+    }
+    update();
+  }
+
+  int checkQuantity(int i) {
+    if (i < 0) {
+      return 0;
+    } else if (i > 20) {
+      return 20;
+    }
+    return i;
+  }
+
+  void addCartItem(ProductModel productModel) {
+    if (_quantity < 1) {
+      _cartController.tryRemoveItem(productModel);
+    } else {
+      _cartController.addItem(productModel, _quantity);
+    }
+    update();
+  }
 }
--- lib/controllers/popular_product_controller.dart
+++ lib/controllers/popular_product_controller.dart
@@ -3,25 +3,17 @@ import 'package:food_delivery/data/repo/popular_product_repo.dart';
 import 'package:food_delivery/models/product_model.dart';
 import 'package:get/get.dart';
 
-class PopularProductController extends GetxController {
+import 'base_controller.dart';
+
+class PopularProductController extends BaseController {
   final PopularProductRepo popularProductRepo;
 
   PopularProductController({required this.popularProductRepo});
   List<ProductModel> _popularProductList = [];
   List<ProductModel> get getPopularProductList => _popularProductList;
 
-  final CartController _cartController = Get.find<CartController>();
-
   bool _isLoaded = false;
   bool get getIsLoaded => _isLoaded;
-
-  int _cartItems = 0;
-  int _quantity = 0;
-
-  int get getCartItems => _cartItems + _quantity;
-  int get getTotalCartItemsQty => _cartController.getTotalQuantities();
-
-  String get getQuantity => '$_quantity';
 
   @override
   Future<void> onInit() async {
@@ -39,37 +31,5 @@ class PopularProductController extends GetxController {
     } else {
       //      throw Exception("Error");
     }
-  }
-
-  void setQuantity(bool isIncrement) {
-    if (isIncrement) {
-      _quantity = checkQuantity(_quantity + 1);
-    } else {
-      _quantity = checkQuantity(_quantity - 1);
-    }
-    update();
-  }
-
-  int checkQuantity(int i) {
-    if (i < 0) {
-      return 0;
-    } else if (i > 20) {
-      return 20;
-    }
-    return i;
-  }
-
-  void initProduct(ProductModel p) {
-    _cartItems = 0;
-    _quantity = _cartController.getQuantity(p);
-  }
-
-  void addCartItem(ProductModel productModel) {
-    if (_quantity < 1) {
-      _cartController.tryRemoveItem(productModel);
-    } else {
-      _cartController.addItem(productModel, _quantity);
-    }
-    update();
   }
 }
