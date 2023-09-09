145ae04863716e5f7c64a440b5b08e09f6cb944c
Pascal JK <pascual.santos80@gmail.com>
2023-09-09 18:28:47 +0200

    - added navigation to cart page.

--- lib/pages/food/popular_food_detail.dart
+++ lib/pages/food/popular_food_detail.dart
@@ -4,6 +4,7 @@ import 'package:food_delivery/components/app_icon.dart';
 import 'package:food_delivery/components/text/big.dart';
 import 'package:food_delivery/components/text/expandable_text.dart';
 import 'package:food_delivery/controllers/popular_product_controller.dart';
+import 'package:food_delivery/routes/route_helper.dart';
 import 'package:food_delivery/utils/app_constants.dart';
 import 'package:food_delivery/utils/colors.dart';
 import 'package:food_delivery/utils/dimensions.dart';
@@ -51,6 +52,7 @@ class PopularFoodDetail extends StatelessWidget {
                     icon: Icons.shopping_cart_outlined,
                     useBadge: true,
                     text: '${c.getTotalCartItemsQty}',
+                    onPressed: () => Get.toNamed(RouteHelper.cart),
                   );
                 }),
               ],
