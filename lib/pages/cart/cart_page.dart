a0066d533362166d95a203ed1f3105bf76d5eace
Pascal JK <pascual.santos80@gmail.com>
2023-09-09 18:29:00 +0200

    - working on cart page ui.

--- lib/pages/cart/cart_page.dart
+++ lib/pages/cart/cart_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/utils/colors.dart';
+import 'package:food_delivery/utils/dimensions.dart';
+import 'package:get/get.dart';
+
+import '../../components/app_icon.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
@@ -6,7 +11,42 @@ class CartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: Stack(
+        children: [
+          Positioned(
+            top: Dimensions.height20 * 3,
+            left: Dimensions.height20,
+            right: Dimensions.height20,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                AppIcon(
+                  icon: Icons.arrow_back,
+                  iconColor: Colors.white,
+                  bgColor: AppColors.mainColor,
+                  iconSize: Dimensions.iconSize24,
+                  onPressed: () => Get.back(),
+                ),
+                SizedBox(width: Dimensions.width20 * 5),
+                AppIcon(
+                  icon: Icons.home_outlined,
+                  iconColor: Colors.white,
+                  bgColor: AppColors.mainColor,
+                  iconSize: Dimensions.iconSize24,
+                  onPressed: () => Get.back(),
+                ),
+                AppIcon(
+                  icon: Icons.shopping_cart,
+                  iconColor: Colors.white,
+                  bgColor: AppColors.mainColor,
+                  iconSize: Dimensions.iconSize24,
+                  onPressed: () => Get.back(),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
