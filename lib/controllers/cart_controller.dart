8fe7d46fd6ebe29e557d0d1e7b04a8ca36b2ed1b
Pascal JK <pascual.santos80@gmail.com>
2023-09-09 13:40:46 +0200

    - working on controller logic

--- lib/controllers/cart_controller.dart
+++ lib/controllers/cart_controller.dart
@@ -1,5 +1,8 @@
+import 'package:food_delivery/data/repo/cart_repo.dart';
 import 'package:get/get.dart';
 
 class CartController extends GetxController {
-  
+  final CartRepo cartRepo;
+
+  CartController({required this.cartRepo});
 }
