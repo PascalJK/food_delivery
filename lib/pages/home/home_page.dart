c2e990f23a6b4d516c14cd7ecf637ed43d9d73f2
Pascal JK <pascual.santos80@gmail.com>
2023-09-13 20:05:47 +0200

    - replaced default bottom nav bar with persistent nav bar package.

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:food_delivery/pages/home/main_food_page.dart';
 import 'package:food_delivery/utils/colors.dart';
+import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,7 +12,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _selectedIndex = 0;
+  final _controller = PersistentTabController(initialIndex: 0);
 
   final List<Widget> _pages = [
     const MainFoodPage(),
@@ -19,30 +21,64 @@ class _HomePageState extends State<HomePage> {
     const Center(child: Text('Test Page_3')),
   ];
 
-  final List<BottomNavigationBarItem> _barItems = [
-    BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: 'Home'),
-    BottomNavigationBarItem(icon: Icon(Icons.history), label: 'History'),
-    BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: 'Cart'),
-    BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Account'),
+  final List<PersistentBottomNavBarItem> _barItems = [
+    PersistentBottomNavBarItem(
+      icon: const Icon(Icons.home_outlined),
+      title: 'Home',
+      activeColorPrimary: AppColors.mainColor,
+      inactiveColorPrimary: CupertinoColors.systemGrey,
+    ),
+    PersistentBottomNavBarItem(
+      icon: const Icon(Icons.history),
+      title: 'History',
+      activeColorPrimary: AppColors.mainColor,
+      inactiveColorPrimary: CupertinoColors.systemGrey,
+    ),
+    PersistentBottomNavBarItem(
+      icon: const Icon(Icons.shopping_cart),
+      title: 'Cart',
+      activeColorPrimary: AppColors.mainColor,
+      inactiveColorPrimary: CupertinoColors.systemGrey,
+    ),
+    PersistentBottomNavBarItem(
+      icon: const Icon(Icons.person),
+      title: 'Account',
+      activeColorPrimary: AppColors.mainColor,
+      inactiveColorPrimary: CupertinoColors.systemGrey,
+    ),
   ];
-
-  void _onTapNav(int index) => setState(() => _selectedIndex = index);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _pages[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: _onTapNav,
-        currentIndex: _selectedIndex,
-        selectedItemColor: AppColors.mainColor,
-        unselectedItemColor: Colors.amberAccent,
-        selectedFontSize: 0,
-        unselectedFontSize: 0,
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        items: _barItems,
+    return PersistentTabView(
+      context,
+      controller: _controller,
+      screens: _pages,
+      items: _barItems,
+      confineInSafeArea: true,
+      backgroundColor: Colors.white, // Default is Colors.white.
+      handleAndroidBackButtonPress: true, // Default is true.
+      resizeToAvoidBottomInset: true,
+      stateManagement: true, // Default is true.
+      hideNavigationBarWhenKeyboardShows: true,
+      decoration: NavBarDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        colorBehindNavBar: Colors.white,
       ),
+      popAllScreensOnTapOfSelectedTab: true,
+      popActionScreens: PopActionScreensType.all,
+      itemAnimationProperties: ItemAnimationProperties(
+        // Navigation Bar's items animation properties.
+        duration: Duration(milliseconds: 200),
+        curve: Curves.ease,
+      ),
+      screenTransitionAnimation: ScreenTransitionAnimation(
+        // Screen transition animation on change of selected tab.
+        animateTabTransition: true,
+        curve: Curves.ease,
+        duration: Duration(milliseconds: 450),
+      ),
+      navBarStyle: NavBarStyle.style6, // Choose the nav bar style with this property.
     );
   }
 }
