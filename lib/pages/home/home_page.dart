65961cb0b8e46a8aa399c593f196330c1099404a
Pascal JK <pascual.santos80@gmail.com>
2023-09-13 19:11:33 +0200

    - working on homepage content.

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/pages/home/main_food_page.dart';
+import 'package:food_delivery/utils/colors.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,8 +10,36 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = [
+    const MainFoodPage(),
+    const Center(child: Text('Test Page_1')),
+    const Center(child: Text('Test Page_2')),
+    const Center(child: Text('Test Page_3')),
+  ];
+
+  final List<BottomNavigationBarItem> _barItems = [
+    BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: 'Home'),
+    BottomNavigationBarItem(icon: Icon(Icons.history), label: 'History'),
+    BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: 'Cart'),
+    BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Account'),
+  ];
+
+  void _onTapNav(int index) => setState(() => _selectedIndex = index);
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _onTapNav,
+        selectedItemColor: AppColors.mainColor,
+        unselectedItemColor: Colors.amberAccent,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        items: _barItems,
+      ),
+    );
   }
 }
