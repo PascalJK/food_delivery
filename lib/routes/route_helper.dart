1c21c01611b96025193e0ebc791fca1598e002a5
Pascal JK <pascual.santos80@gmail.com>
2023-09-14 13:01:15 +0200

    - registering splash screen route.

--- lib/routes/route_helper.dart
+++ lib/routes/route_helper.dart
@@ -2,10 +2,12 @@ import 'package:food_delivery/pages/cart/cart_page.dart';
 import 'package:food_delivery/pages/food/popular_food_detail.dart';
 import 'package:food_delivery/pages/food/recommended_food_detail.dart';
 import 'package:food_delivery/pages/home/home_page.dart';
+import 'package:food_delivery/pages/splash/splash_page.dart';
 import 'package:get/route_manager.dart';
 
 class RouteHelper {
   static const initial = '/';
+  static const splash = '/splash-page';
   static const popularFood = '/popular-food';
   static const recommendedFood = '/recommended-food';
   static const cart = '/cart';
@@ -20,6 +22,11 @@ class RouteHelper {
     GetPage(
       name: initial,
       page: () => const HomePage(),
+    ),
+    // Splash-Page
+    GetPage(
+      name: splash,
+      page: () => const SplashScreen(),
     ),
     // Popular Food Detail
     GetPage(
