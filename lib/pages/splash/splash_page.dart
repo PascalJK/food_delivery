e2b6d69d7ba2221a6a43280277d4b2d7c6d6e331
Pascal JK <pascual.santos80@gmail.com>
2023-09-14 13:03:42 +0200

    - working on splash page ui

--- lib/pages/splash/splash_page.dart
+++ lib/pages/splash/splash_page.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:food_delivery/routes/route_helper.dart';
+import 'package:get/get.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -9,14 +13,20 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMixin {
   late var animation = CurvedAnimation(parent: controller, curve: Curves.linear);
-  late var controller = AnimationController(vsync: this, duration: const Duration(seconds: 2));
+  late var controller = AnimationController(vsync: this, duration: const Duration(seconds: 2))..forward();
+  final timer = Timer(const Duration(seconds: 3), () => Get.offAllNamed(RouteHelper.initial));
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Image.asset('assets/image/logo part 1.png'),
+          ScaleTransition(
+              scale: animation,
+              child: Center(child: Image.asset('assets/image/logo part 1.png', width: 250))),
+          Center(child: Image.asset('assets/image/logo part 2.png', width: 250)),
         ],
       ),
     );
