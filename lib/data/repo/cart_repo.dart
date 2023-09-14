21217600ac41b08e1b5a91aee5e1115cae4a7cf7
Pascal JK <pascual.santos80@gmail.com>
2023-09-14 15:17:54 +0200

    - working on cart repo logic.

--- lib/data/repo/cart_repo.dart
+++ lib/data/repo/cart_repo.dart
@@ -1,7 +1,13 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
+import '../../models/cart_model.dart';
+
 class CartRepo {
   final SharedPreferences prefs;
 
   CartRepo({required this.prefs});
+
+  void addToCartList(List<CartModel> list){
+    
+  }
 }
