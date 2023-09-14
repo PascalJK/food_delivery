ba889d441fffd2097a3de132ddf7733dcbf3a623
Pascal JK <pascual.santos80@gmail.com>
2023-09-14 22:58:49 +0200

    - working on displaying cart list data.

--- lib/data/repo/cart_repo.dart
+++ lib/data/repo/cart_repo.dart
@@ -16,4 +16,14 @@ class CartRepo {
     }
     prefs.setStringList(AppConstants.CART_LIST, cart);
   }
+
+  List<CartModel> getCartList() {
+    List<CartModel> list = [];
+    if (prefs.containsKey(AppConstants.CART_LIST)) {
+      prefs.getStringList(AppConstants.CART_LIST)?.forEach((e) {
+        list.add(CartModel.fromJson(e));
+      });
+    }
+    return list;
+  }
 }
