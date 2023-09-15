f75d754e69a49a25aaf58edee353d7ceeb436118
Pascal JK <pascual.santos80@gmail.com>
2023-09-15 18:20:55 +0200

    - working on cart history page.

--- lib/pages/cart/cart_history.dart
+++ lib/pages/cart/cart_history.dart
@@ -1,10 +1,16 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:food_delivery/components/text/big.dart';
 
 class CartHistory extends StatelessWidget {
   const CartHistory({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: BigText(text: 'Cart History'),
+      ),
+      body: Column(),
+    );
   }
 }
