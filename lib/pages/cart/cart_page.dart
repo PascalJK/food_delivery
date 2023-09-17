d5a28851f7fbb7b68b550a83726c29f95a22015e
Pascal JK <pascual.santos80@gmail.com>
2023-09-17 21:42:37 +0200

    - working on cart item pressed action.

--- lib/pages/cart/cart_page.dart
+++ lib/pages/cart/cart_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:food_delivery/components/text/big.dart';
 import 'package:food_delivery/components/text/small.dart';
 import 'package:food_delivery/controllers/cart_controller.dart';
+import 'package:food_delivery/models/cart_model.dart';
 import 'package:food_delivery/routes/route_helper.dart';
 import 'package:food_delivery/utils/app_constants.dart';
 import 'package:food_delivery/utils/colors.dart';
@@ -67,70 +68,7 @@ class CartPage extends StatelessWidget {
                     itemCount: c.getCartList.length,
                     itemBuilder: (context, index) {
                       final item = c.getCartList[index];
-                      return Container(
-                        height: 100,
-                        width: double.maxFinite,
-                        margin: const EdgeInsets.only(bottom: 8),
-                        child: Row(
-                          children: [
-                            Container(
-                              height: Dimensions.height20 * 5,
-                              width: Dimensions.height20 * 5,
-                              margin: EdgeInsets.only(bottom: Dimensions.height10),
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(Dimensions.radius20),
-                                color: Colors.white,
-                                image: DecorationImage(
-                                  image: NetworkImage(AppConstants.UPLOAD_URL + item.img!),
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                            ),
-                            SizedBox(width: Dimensions.width10),
-                            Expanded(
-                                child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: [
-                                BigText(text: item.name!),
-                                SmallText(text: 'text'),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    BigText(text: '\$${item.price!}'),
-                                    Container(
-                                      padding: EdgeInsets.symmetric(
-                                        horizontal: Dimensions.height10,
-                                        vertical: Dimensions.height10,
-                                      ),
-                                      decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.circular(Dimensions.radius20),
-                                        color: Colors.white,
-                                      ),
-                                      child: Row(
-                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          GestureDetector(
-                                            onTap: () => c.updateCartItemQty(item, -1),
-                                            child: Icon(Icons.remove, color: AppColors.signColor),
-                                          ),
-                                          SizedBox(width: Dimensions.width10 / 2),
-                                          BigText(text: '${item.quantity}'),
-                                          SizedBox(width: Dimensions.width10 / 2),
-                                          GestureDetector(
-                                            onTap: () => c.updateCartItemQty(item, 1),
-                                            child: Icon(Icons.add, color: AppColors.signColor),
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                  ],
-                                )
-                              ],
-                            ))
-                          ],
-                        ),
-                      );
+                      return _cartItemView(item, c);
                     },
                   );
                 }),
@@ -179,6 +117,89 @@ class CartPage extends StatelessWidget {
               )
             ],
           ),
+        ),
+      ),
+    );
+  }
+
+  Widget _cartItemView(CartModel item, CartController c) {
+    return GestureDetector(
+      onTap: () {
+        // var index =
+        //     Get.find<PopularProductController>().getPopularProductList.indexOf(item.product!);
+        // if (index >= 0) {
+        //   Get.toNamed(RouteHelper.getPopularFood(index));
+        // } else if (Get.find<RecommendedProductController>()
+        //     .getRecommendedProductList
+        //     .contains(item.product)) {
+        //   Get.toNamed(RouteHelper.recommendedFood, arguments: item.product);
+        // } else {
+        Get.toNamed(RouteHelper.recommendedFood, arguments: item.product);
+        //   print('404 not found..');
+        // }
+      },
+      child: Container(
+        height: Dimensions.containerHieght100,
+        width: double.maxFinite,
+        margin: const EdgeInsets.only(bottom: 8),
+        child: Row(
+          children: [
+            Container(
+              height: Dimensions.containerHieght100,
+              width: Dimensions.containerHieght100,
+              margin: EdgeInsets.only(bottom: Dimensions.height10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(Dimensions.radius20),
+                color: Colors.white,
+                image: DecorationImage(
+                  image: NetworkImage(AppConstants.UPLOAD_URL + item.img!),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            SizedBox(width: Dimensions.width10),
+            Expanded(
+                child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                BigText(text: item.name!),
+                SmallText(text: 'text'),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    BigText(text: '\$${item.price!}'),
+                    Container(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: Dimensions.height10,
+                        vertical: Dimensions.height10,
+                      ),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(Dimensions.radius20),
+                        color: Colors.white,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          GestureDetector(
+                            onTap: () => c.updateCartItemQty(item, -1),
+                            child: Icon(Icons.remove, color: AppColors.signColor),
+                          ),
+                          SizedBox(width: Dimensions.width10 / 2),
+                          BigText(text: '${item.quantity}'),
+                          SizedBox(width: Dimensions.width10 / 2),
+                          GestureDetector(
+                            onTap: () => c.updateCartItemQty(item, 1),
+                            child: Icon(Icons.add, color: AppColors.signColor),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ))
+          ],
         ),
       ),
     );
