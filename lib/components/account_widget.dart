5e57b7392a0bd6c0b07d24b27fd7806aa3ea98e9
Pascal JK <pascual.santos80@gmail.com>
2023-09-20 21:58:08 +0200

    - working on account widget content.

--- lib/components/account_widget.dart
+++ lib/components/account_widget.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
 
+import '../utils/dimensions.dart';
+import 'app_icon.dart';
+import 'text/big.dart';
+
 class AccountWidget extends StatelessWidget {
-  const AccountWidget({super.key});
+  const AccountWidget({
+    super.key,
+    required this.iconData,
+    required this.bgColor,
+    required this.text,
+  });
+  final IconData iconData;
+  final Color bgColor;
+  final String text;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      width: double.maxFinite,
+      padding: EdgeInsets.fromLTRB(
+        Dimensions.width20,
+        Dimensions.width10,
+        0,
+        Dimensions.width10,
+      ),
+      decoration: BoxDecoration(color: Colors.white, boxShadow: [
+        BoxShadow(
+          blurRadius: 1,
+          offset: const Offset(0, 2),
+          color: Colors.grey.withOpacity(.2),
+        ),
+      ]),
+      child: Row(
+        children: [
+          AppIcon(
+            icon: iconData,
+            bgColor: bgColor,
+            iconColor: Colors.white,
+            iconSize: Dimensions.iconSize24,
+            size: Dimensions.iconSize24 * 2,
+          ),
+          SizedBox(width: Dimensions.width10),
+          BigText(text: text)
+        ],
+      ),
+    );
   }
 }
