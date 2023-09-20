528f2e5e8b0b285e6cbbbc7f3567eb24cf7ac29b
Pascal JK <pascual.santos80@gmail.com>
2023-09-20 22:01:49 +0200

    - [late commit] updated app icon to use badge.

--- lib/components/app_icon.dart
+++ lib/components/app_icon.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:food_delivery/components/text/big.dart';
 import 'package:food_delivery/utils/colors.dart';
 import 'package:food_delivery/utils/dimensions.dart';
@@ -27,19 +26,19 @@ class AppIcon extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return useBadge
+        ? _badgeButton()
+        : SizedBox(
+            height: size ?? Dimensions.height40,
+            width: size ?? Dimensions.height40,
+            child: _normalButton(iSize: 4));
+  }
+
+  Stack _badgeButton() {
     return Stack(
       alignment: Alignment.topRight,
       children: [
-        MaterialButton(
-          padding: EdgeInsets.zero,
-          height: size ?? Dimensions.height40,
-          minWidth: size ?? Dimensions.height40,
-          shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(size ?? Dimensions.height40 / 2))),
-          color: bgColor ?? const Color(0xfffcf4e4),
-          onPressed: onPressed ?? () {},
-          child: Center(child: _buildIcon()),
-        ),
+        _normalButton(),
         // Badge Widget
         if (useBadge) ...[
           IgnorePointer(
@@ -60,11 +59,21 @@ class AppIcon extends StatelessWidget {
     );
   }
 
-  Icon _buildIcon() {
-    return Icon(
-      icon,
-      color: iconColor ?? const Color(0xff756d54),
-      size: iconSize ?? Dimensions.iconSize16,
+  MaterialButton _normalButton({double iSize = 0}) {
+    return MaterialButton(
+      padding: EdgeInsets.zero,
+      height: size ?? Dimensions.height40,
+      minWidth: size ?? Dimensions.height40,
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(size ?? Dimensions.height40 / 2))),
+      color: bgColor ?? const Color(0xfffcf4e4),
+      onPressed: onPressed ?? () {},
+      child: Center(
+          child: Icon(
+        icon,
+        color: iconColor ?? const Color(0xff756d54),
+        size: (iconSize ?? Dimensions.iconSize16) + iSize,
+      )),
     );
   }
 }
