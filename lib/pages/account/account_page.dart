b19cd1a3fe175536e93c8d650070c81e05adb188
Pascal JK <pascual.santos80@gmail.com>
2023-09-20 21:31:35 +0200

    - working on account page content.

--- lib/pages/account/account_page.dart
+++ lib/pages/account/account_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/components/app_icon.dart';
 import 'package:food_delivery/components/text/big.dart';
+import 'package:food_delivery/utils/colors.dart';
+import 'package:food_delivery/utils/dimensions.dart';
 
 class AccountPage extends StatelessWidget {
   const AccountPage({super.key});
@@ -8,9 +11,27 @@ class AccountPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: BigText(
+        backgroundColor: AppColors.mainColor,
+        centerTitle: true,
+        title: const BigText(
           text: 'Profile',
+          color: Colors.white,
           size: 24,
+        ),
+      ),
+      body: Container(
+        margin: EdgeInsets.only(top: Dimensions.height15),
+        width: double.maxFinite,
+        child: Column(
+          children: [
+            AppIcon(
+              icon: Icons.person,
+              bgColor: AppColors.mainColor,
+              iconColor: Colors.white,
+              iconSize: 75,
+              size: 150,
+            )
+          ],
         ),
       ),
     );
