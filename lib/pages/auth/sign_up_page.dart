38108db3eb92644a894ad52e4c3979545ea9c42b
Pascal JK <pascual.santos80@gmail.com>
2023-09-20 23:45:53 +0200

    - working on page content

--- lib/pages/auth/sign_up_page.dart
+++ lib/pages/auth/sign_up_page.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/utils/dimensions.dart';
 
 class SignUpPage extends StatelessWidget {
   const SignUpPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        children: [
+          SizedBox(height: Dimensions.screenHeight*.05),
+          // Image
+          SizedBox(
+            height: Dimensions.screenHeight*.25,
+            child: const Center(
+              child: CircleAvatar(
+                backgroundColor: Colors.white,
+                radius: 80,
+                backgroundImage: AssetImage('assets/image/logo_part_1.png'),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
