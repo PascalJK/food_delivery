7229a6d006d972bf66481f21a50b454c68089cdd
Pascal JK <pascual.santos80@gmail.com>
2023-09-22 01:36:47 +0200

    - working on sign up page interface

--- lib/pages/auth/sign_up_page.dart
+++ lib/pages/auth/sign_up_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:food_delivery/components/text/text_input_field.dart';
 import 'package:food_delivery/utils/dimensions.dart';
@@ -10,46 +11,88 @@ class SignUpPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var images = ['t.png', 'f.png', 'g.png'];
+
     return Scaffold(
-      body: Column(
-        children: [
-          SizedBox(height: Dimensions.screenHeight * .05),
-          // Image
-          SizedBox(
-            height: Dimensions.screenHeight * .25,
-            child: const Center(
-              child: CircleAvatar(
-                backgroundColor: Colors.white,
-                radius: 80,
-                backgroundImage: AssetImage('assets/image/logo part 1.png'),
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Column(
+          children: [
+            SizedBox(height: Dimensions.screenHeight * .05),
+            // Image
+            SizedBox(
+              height: Dimensions.screenHeight * .25,
+              child: const Center(
+                child: CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius: 80,
+                  backgroundImage: AssetImage('assets/image/logo part 1.png'),
+                ),
               ),
             ),
-          ),
-          // Input-fields
-          const TextInputField(controller: null, hint: 'Email', icon: Icons.email),
-          SizedBox(height: Dimensions.height15),
-          const TextInputField(controller: null, hint: 'Phone', icon: Icons.phone),
-          SizedBox(height: Dimensions.height15),
-          const TextInputField(controller: null, hint: 'Name', icon: Icons.person),
-          SizedBox(height: Dimensions.height15),
-          const TextInputField(controller: null, hint: 'Password', icon: Icons.password),
-          SizedBox(height: Dimensions.height30),
-          MaterialButton(
-            padding: EdgeInsets.symmetric(
-              horizontal: Dimensions.height20,
-              vertical: Dimensions.height20,
+            // Input-fields
+            const TextInputField(controller: null, hint: 'Email', icon: Icons.email),
+            SizedBox(height: Dimensions.height15),
+            const TextInputField(controller: null, hint: 'Phone', icon: Icons.phone),
+            SizedBox(height: Dimensions.height15),
+            const TextInputField(controller: null, hint: 'Name', icon: Icons.person),
+            SizedBox(height: Dimensions.height15),
+            const TextInputField(controller: null, hint: 'Password', icon: Icons.password),
+            // Sign up button
+            SizedBox(height: Dimensions.height30),
+            MaterialButton(
+              padding: EdgeInsets.symmetric(
+                horizontal: Dimensions.height20,
+                vertical: Dimensions.height20,
+              ),
+              minWidth: Dimensions.screenWidth * .8,
+              onPressed: () {},
+              color: AppColors.mainColor,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(Dimensions.radius15))),
+              child: const BigText(
+                text: 'Sign up',
+                color: Colors.white,
+              ),
             ),
-            minWidth: Dimensions.screenWidth,
-            onPressed: () {},
-            color: AppColors.mainColor,
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(Dimensions.radius15))),
-            child: const BigText(
-              text: 'Checkout',
-              color: Colors.white,
+            // already has account text
+            SizedBox(height: Dimensions.height15),
+            RichText(
+              text: TextSpan(
+                text: 'Already have an account?',
+                recognizer: TapGestureRecognizer()..onTap = () => print('tap'),
+                style: TextStyle(
+                  color: Colors.grey[500],
+                  fontSize: Dimensions.font17,
+                ),
+              ),
             ),
-          )
-        ],
+            // Sign up options
+            SizedBox(height: Dimensions.screenHeight * .03),
+            RichText(
+              text: TextSpan(
+                text: 'Sign up using one of the following methods',
+                style: TextStyle(
+                  color: Colors.grey[500],
+                  fontSize: Dimensions.font12,
+                ),
+              ),
+            ),
+            SizedBox(height: Dimensions.screenHeight * .05),
+            Wrap(
+              children: List.generate(
+                images.length,
+                (index) => Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8),
+                  child: CircleAvatar(
+                    radius: Dimensions.radius20 + 5,
+                    backgroundImage: AssetImage('assets/image/${images[index]}'),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
