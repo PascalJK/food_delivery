00a45ba1e1fc37d8d00bd9481d6beb3a52c8c2bf
Pascal JK <pascual.santos80@gmail.com>
2023-09-22 02:44:50 +0200

    - extended Input validator mixin

--- lib/controllers/sign_up_controller.dart
+++ lib/controllers/sign_up_controller.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/utils/validators/input_validators.dart';
 import 'package:get/get.dart';
 
-class SignUpController extends GetxController {
+class SignUpController extends GetxController with InputValidator{
   final email = TextEditingController();
   final name = TextEditingController();
   final contact = TextEditingController();
