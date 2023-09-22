98092c2aec9b8df6dedef1a0da2a76c4711b3056
Pascal JK <pascual.santos80@gmail.com>
2023-09-22 21:27:07 +0200

    - working on model properties.

--- lib/models/account_model.dart
+++ lib/models/account_model.dart
@@ -1,2 +1,12 @@
 class AccountModel {
+  String name;
+  String contact;
+  String email;
+  String password;
+  AccountModel({
+    required this.name,
+    required this.contact,
+    required this.email,
+    required this.password,
+  });
 }
