5c8599a6989048a1086ba20c3d5bb7a847f5f4c4
Pascal JK <pascual.santos80@gmail.com>
2023-09-22 02:29:49 +0200

    - working on controller properties

--- lib/controllers/sign_up_controller.dart
+++ lib/controllers/sign_up_controller.dart
@@ -1,4 +1,9 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class SignUpController extends GetxController {
+  final email = TextEditingController();
+  final name = TextEditingController();
+  final contact = TextEditingController();
+  final password = TextEditingController();
 }
