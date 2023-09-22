9dd633701a63b3d547f541d0a821cdf01f32aef9
Pascal JK <pascual.santos80@gmail.com>
2023-09-22 02:00:02 +0200

    - working on sign in page ui

--- lib/pages/auth/sign_in_page.dart
+++ lib/pages/auth/sign_in_page.dart
@@ -1,10 +1,124 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:food_delivery/pages/auth/sign_up_page.dart';
+import 'package:get/get.dart';
+
+import '../../components/text/big.dart';
+import '../../components/text/text_input_field.dart';
+import '../../utils/colors.dart';
+import '../../utils/dimensions.dart';
 
 class SignInPage extends StatelessWidget {
   const SignInPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Column(
+          children: [
+            SizedBox(height: Dimensions.screenHeight * .05),
+            // Image
+            SizedBox(
+              height: Dimensions.screenHeight * .25,
+              child: const Center(
+                child: CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius: 80,
+                  backgroundImage: AssetImage('assets/image/logo part 1.png'),
+                ),
+              ),
+            ),
+            // welcome
+            Container(
+              margin: EdgeInsets.only(left: Dimensions.width20),
+              width: double.maxFinite,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Hello',
+                    style: TextStyle(
+                      fontSize: Dimensions.font20 * 3 + Dimensions.font20 / 2,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    'Sign into your account',
+                    style: TextStyle(
+                      fontSize: Dimensions.font20,
+                      color: Colors.grey[500],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            // Input-fields
+            SizedBox(height: Dimensions.height20),
+            const TextInputField(controller: null, hint: 'Email', icon: Icons.email),
+            SizedBox(height: Dimensions.height15),
+            const TextInputField(controller: null, hint: 'Password', icon: Icons.password),
+            // Sign in button
+            SizedBox(height: Dimensions.height30),
+            MaterialButton(
+              padding: EdgeInsets.symmetric(
+                horizontal: Dimensions.height20,
+                vertical: Dimensions.height20,
+              ),
+              minWidth: Dimensions.screenWidth * .8,
+              onPressed: () {},
+              color: AppColors.mainColor,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(Dimensions.radius15))),
+              child: const BigText(
+                text: 'Sign in',
+                color: Colors.white,
+              ),
+            ),
+            // forgot password
+            SizedBox(height: Dimensions.height15),
+            Padding(
+              padding: EdgeInsets.only(right: Dimensions.width15),
+              child: Align(
+                alignment: Alignment.topRight,
+                child: RichText(
+                  text: TextSpan(
+                    text: 'Forgot Password?',
+                    recognizer: TapGestureRecognizer()..onTap = () => print('tap'),
+                    style: TextStyle(
+                      color: Colors.grey[500],
+                      fontSize: Dimensions.font17,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            // Sign up options
+            SizedBox(height: Dimensions.screenHeight * .03),
+            RichText(
+              text: TextSpan(
+                  text: 'Don\'t have an account? ',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                    fontSize: Dimensions.font20,
+                  ),
+                  children: [
+                    TextSpan(
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () => Get.to(() => const SignUpPage()),
+                      text: 'Create',
+                      style: TextStyle(
+                          color: AppColors.mainBlackColor,
+                          fontSize: Dimensions.font20,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ]),
+            ),
+            SizedBox(height: Dimensions.screenHeight * .05),
+          ],
+        ),
+      ),
+    );
   }
 }
