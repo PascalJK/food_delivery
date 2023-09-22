1485e875e96252e53a82cb57a40986b8dfd4c661
Pascal JK <pascual.santos80@gmail.com>
2023-09-22 02:43:46 +0200

    - added new validator file

---
+++ lib/utils/validators/input_validators.dart
@@ -0,0 +1,36 @@
+import 'package:get/get.dart';
+
+mixin InputValidator{
+  // Validators
+  String? validateEmptyField(String value) {
+    if (value.isEmpty) return "Field cannot be empty.";
+    return null;
+  }
+
+  String? validateContactNumber(String value) {
+    if (value.isEmpty) return "Type in contact number.";
+    if (!GetUtils.isPhoneNumber(value)) return "Enter a valid phone number.";
+    return null;
+  }
+
+  String? validatePrice(String value) {
+    if (value.isEmpty) return "Type in price.";
+    if (!GetUtils.isNum(value)) return "Enter a valid price.";
+    return null;
+  }
+
+  String? validateEmail(String value) {
+    if (value.isEmpty) return 'Type in your email address';
+    if (!GetUtils.isEmail(value.trim())) return "Enter a valid email address.";
+    return null;
+  }
+
+  String? validateConfirmationPassword(String password1, String password2) {
+    if (password1.isEmpty) return "Type in your password.";
+    if (password1.length < 4) return "Password can not be less than (6) characters";
+
+    // Compare Passwords
+    if (password2 != password1) return "Passwords don't match";
+    return null;
+  }
+}
