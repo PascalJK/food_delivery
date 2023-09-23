26d040d62bdb1db9c5602723891bf8b056fb9703
Pascal JK <pascual.santos80@gmail.com>
2023-09-24 00:18:04 +0200

    - working on auth controller logic.

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,9 +1,29 @@
+import 'package:food_delivery/models/response_model.dart';
 import 'package:get/get.dart';
 
 import '../data/repo/auth_repo.dart';
+import '../models/account_model.dart';
 
 class AuthController extends GetxController {
   final AuthRepo authRepo;
 
   AuthController({required this.authRepo});
+
+  bool _isloading = false;
+  bool get isLoading => _isloading;
+
+  registration(AccountModel account) async {
+    _isloading = true;
+    var res = await authRepo.registration(account);
+    late ResponseModel responseModel;
+    if (res.statusCode == 200) {
+      authRepo.saveUserToken(res.body['token']);
+      responseModel = ResponseModel(true, res.body['token']);
+    } else {
+      responseModel = ResponseModel(true, res.body['token']);
+    }
+    _isloading = false;
+    update();
+    return responseModel;
+  }
 }
