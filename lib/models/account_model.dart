8930cf1b8d332784870bacefd7f8a61732d9ba09
Pascal JK <pascual.santos80@gmail.com>
2023-09-23 22:24:31 +0200

    - working on account json.

--- lib/models/account_model.dart
+++ lib/models/account_model.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class AccountModel {
   String name;
   String contact;
@@ -9,4 +11,24 @@ class AccountModel {
     required this.email,
     required this.password,
   });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'name': name,
+      'contact': contact,
+      'email': email,
+      'password': password,
+    };
+  }
+
+  factory AccountModel.fromMap(Map<String, dynamic> map) {
+    return AccountModel(
+      name: map['name'] as String,
+      contact: map['contact'] as String,
+      email: map['email'] as String,
+      password: map['password'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
 }
