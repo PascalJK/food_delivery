e4157704e1e6c3cd6384f3e3cba98a389b1d33df
Pascal JK <pascual.santos80@gmail.com>
2023-09-23 22:06:43 +0200

    - working on repo properties

--- lib/data/repo/auth_repo.dart
+++ lib/data/repo/auth_repo.dart
@@ -1,3 +1,10 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../api/api_client.dart';
+
 class AuthRepo {
-  
+  final ApiClient apiClient;
+  final SharedPreferences preferences;
+
+  AuthRepo({required this.apiClient, required this.preferences});
 }
