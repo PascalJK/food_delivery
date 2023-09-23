27b6503030d99097c8aad08eb713b98880e54ec6
Pascal JK <pascual.santos80@gmail.com>
2023-09-24 00:27:46 +0200

    - creating communication btw sign up & auth controllers.

--- lib/controllers/sign_up_controller.dart
+++ lib/controllers/sign_up_controller.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery/components/custom_snackbar.dart';
-// import 'package:food_delivery/controllers/auth_controller.dart';
+import 'package:food_delivery/controllers/auth_controller.dart';
 import 'package:food_delivery/utils/validators/input_validators.dart';
 import 'package:get/get.dart';
 
-class SignUpController extends /*AuthController*/ GetxController with InputValidator {
+import '../models/account_model.dart';
+
+class SignUpController extends GetxController with InputValidator {
   final email = TextEditingController();
   final name = TextEditingController();
   final contact = TextEditingController();
   final password = TextEditingController();
 
-  void registration() {
+  final _authController = Get.find<AuthController>();
+
+  Future<void> registration() async {
     if (validateEmail(email.text) != null) {
       showCustomSnackBar(validateEmail(email.text)!, title: 'Email');
     } else if (validateEmptyField(name.text) != null) {
@@ -21,12 +25,19 @@ class SignUpController extends /*AuthController*/ GetxController with InputValid
       showCustomSnackBar(validatePassword(password.text)!, title: 'Password');
     } else {
       showCustomSnackBar('All went well', title: 'Success');
-      // var acc = AccountModel(
-      //   name: name.text,
-      //   contact: contact.text,
-      //   email: email.text,
-      //   password: password.text,
-      // );
+      var acc = AccountModel(
+        name: name.text,
+        contact: contact.text,
+        email: email.text,
+        password: password.text,
+      );
+      await _authController.registration(acc).then((status) {
+        if (status.getSuccess) {
+          print('passed test');
+        } else {
+          showCustomSnackBar(status.getMessage);
+        }
+      });
     }
   }
 }
