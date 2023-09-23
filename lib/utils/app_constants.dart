9278e7463e3a9b9ec1e238220376e83b091b8610
Pascal JK <pascual.santos80@gmail.com>
2023-09-23 22:20:41 +0200

    - added new end point

--- lib/utils/app_constants.dart
+++ lib/utils/app_constants.dart
@@ -14,6 +14,7 @@ class AppConstants {
   // static const String BASE_URL = 'https://127.0.0.1:8000';
 
   static const String BASE_URL = 'https://mvs.bslmeiyu.com';
+  static const String REGISTRATION_URI = '/api/v1/auth/register';
   static const String POPULAR_PRODUCT_URI = '/api/v1/products/popular';
   static const String RECOMMENDED_PRODUCT_URI = '/api/v1/products/recommended';
 
