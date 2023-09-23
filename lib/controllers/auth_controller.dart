9834217e36fe7fe773b4111c831e7078cbb635b9
Pascal JK <pascual.santos80@gmail.com>
2023-09-23 22:07:36 +0200

    - working on auth controller

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,5 +1,9 @@
 import 'package:get/get.dart';
 
+import '../data/repo/auth_repo.dart';
+
 class AuthController extends GetxController {
-  
+  final AuthRepo authRepo;
+
+  AuthController({required this.authRepo});
 }
