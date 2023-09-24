ecb8111ddbe7d374b7784407aac3197f7e4f07e5
Pascal JK <pascual.santos80@gmail.com>
2023-09-24 22:49:13 +0200

    - working on file content.

--- lib/components/loading_indicator.dart
+++ lib/components/loading_indicator.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/utils/colors.dart';
+import 'package:food_delivery/utils/dimensions.dart';
 
 class LoadingIndicator extends StatelessWidget {
   const LoadingIndicator({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      height: Dimensions.height20 * 5,
+      width: Dimensions.height20 * 5,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(Dimensions.height20 * 5 / 2),
+        color: AppColors.mainColor,
+      ),
+      alignment: Alignment.center,
+      child: const CircularProgressIndicator(color: Colors.white),
+    );
   }
 }
