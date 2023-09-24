9a0c9a88f74a732e6d12dfea62d0adff1612ecdb
Pascal JK <pascual.santos80@gmail.com>
2023-09-24 22:29:06 +0200

    - removed update

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -23,7 +23,6 @@ class AuthController extends GetxController {
       responseModel = ResponseModel(false, res.statusText!);
     }
     _isloading = false;
-    update();
     return responseModel;
   }
 }
