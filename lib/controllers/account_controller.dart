fae9dacdf688be2138c8eac2e920a3566521459a
Pascal JK <pascual.santos80@gmail.com>
2023-09-25 19:59:08 +0200

    - working on logout method.

--- lib/controllers/account_controller.dart
+++ lib/controllers/account_controller.dart
@@ -1,5 +1,17 @@
+import 'package:food_delivery/controllers/cart_controller.dart';
 import 'package:get/get.dart';
 
+import '../routes/route_helper.dart';
+import 'auth_controller.dart';
+
 class AccountController extends GetxController {
-  
+  void logout() {
+    var authController = Get.find<AuthController>();
+    if (authController.isUserLoggedIn()) {
+      authController.clearSharedData();
+      Get.find<CartController>().clear();
+      Get.find<CartController>().clearCartHistory();
+      Get.offNamed(RouteHelper.login);
+    }
+  }
 }
