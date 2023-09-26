40670facf90a91effc3489c83bfbcdde44e3aef3
Pascal JK <pascual.santos80@gmail.com>
2023-09-26 19:06:02 +0200

    - registered dependencies.

--- lib/helper/dependencies.dart
+++ lib/helper/dependencies.dart
@@ -1,3 +1,4 @@
+import 'package:food_delivery/controllers/account_controller.dart';
 import 'package:food_delivery/controllers/auth_controller.dart';
 import 'package:food_delivery/controllers/cart_controller.dart';
 import 'package:food_delivery/controllers/popular_product_controller.dart';
@@ -6,6 +7,7 @@ import 'package:food_delivery/data/api/api_client.dart';
 import 'package:food_delivery/data/repo/auth_repo.dart';
 import 'package:food_delivery/data/repo/popular_product_repo.dart';
 import 'package:food_delivery/data/repo/recommended_product_repo.dart';
+import 'package:food_delivery/data/repo/user_repo.dart';
 import 'package:food_delivery/utils/app_constants.dart';
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -24,12 +26,13 @@ Future init() async {
   Get.lazyPut(() => RecommendedProductRepo(apiClient: Get.find<ApiClient>()));
   Get.lazyPut(() => PopularProductRepo(apiClient: Get.find<ApiClient>()));
   Get.lazyPut(() => CartRepo(prefs: Get.find<SharedPreferences>()));
+  Get.lazyPut(() => UserRepo(apiClient: Get.find<ApiClient>()));
 
   // controllers
   // Get.lazyPut(() => AuthController(authRepo: Get.find<AuthRepo>()));
   Get.put(AuthController(authRepo: Get.find<AuthRepo>()));
+  Get.lazyPut(() => AccountController(userRepo: Get.find<UserRepo>()));
   Get.lazyPut(() => PopularProductController(popularProductRepo: Get.find<PopularProductRepo>()));
-  Get.lazyPut(() =>
-      RecommendedProductController(recommendedProductRepo: Get.find<RecommendedProductRepo>()));
+  Get.lazyPut(() => RecommendedProductController(recommendedProductRepo: Get.find()));
   Get.lazyPut(() => CartController(cartRepo: Get.find<CartRepo>()));
 }
