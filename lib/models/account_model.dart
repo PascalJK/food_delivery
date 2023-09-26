db4d35bb1db0c4dd54f65f4dc6d35db0cd672990
Pascal JK <pascual.santos80@gmail.com>
2023-09-26 13:26:54 +0200

    - updated account model

--- lib/models/account_model.dart
+++ lib/models/account_model.dart
@@ -1,19 +1,25 @@
 import 'dart:convert';
 
 class AccountModel {
+  int id;
   String name;
   String contact;
   String email;
   String password;
+  int orderCount;
   AccountModel({
+    this.id = 0,
     required this.name,
     required this.contact,
     required this.email,
     required this.password,
+    this.orderCount = 0,
   });
 
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
+      'id': id,
+      'order_count': orderCount,
       'f_name': name,
       'phone': contact,
       'email': email,
@@ -23,10 +29,12 @@ class AccountModel {
 
   factory AccountModel.fromMap(Map<String, dynamic> map) {
     return AccountModel(
+      id: map['id'] as int,
       name: map['f_name'] as String,
       contact: map['phone'] as String,
       email: map['email'] as String,
       password: map['password'] as String,
+      orderCount: map['order_count'] as int,
     );
   }
 
