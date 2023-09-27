28f237937e3ccdfae6f62a8849a5c11051cecb36
Pascal JK <pascual.santos80@gmail.com>
2023-09-27 23:01:51 +0200

    - working on model json

--- lib/models/address_model.dart
+++ lib/models/address_model.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class AddressModel {
   final int? id;
   final String type;
@@ -16,4 +18,32 @@ class AddressModel {
     required this.latitude,
     required this.longitude,
   });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'type': type,
+      'contactPersonName': contactPersonName,
+      'contactPersonNumber': contactPersonNumber,
+      'address': address,
+      'latitude': latitude,
+      'longitude': longitude,
+    };
+  }
+
+  factory AddressModel.fromMap(Map<String, dynamic> map) {
+    return AddressModel(
+      id: map['id'] as int,
+      type: map['type'] ?? '',
+      contactPersonName: map['contactPersonName'] ?? '',
+      contactPersonNumber: map['contactPersonNumber'] ?? '',
+      address: map['address'] ?? '',
+      latitude: map['latitude'] ?? '',
+      longitude: map['longitude'] ?? '',
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory AddressModel.fromJson(String source) => AddressModel.fromMap(json.decode(source) as Map<String, dynamic>);
 }
