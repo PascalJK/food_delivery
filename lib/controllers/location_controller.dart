778b57282f4409c50f66d27a5cb4605f78e9985a
Pascal JK <pascual.santos80@gmail.com>
2023-09-27 23:07:22 +0200

    - working on controller properties.

--- lib/controllers/location_controller.dart
+++ lib/controllers/location_controller.dart
@@ -1,3 +1,24 @@
-class LocationController {
-  
+import 'package:food_delivery/data/repo/location_repo.dart';
+import 'package:food_delivery/models/address_model.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:get/get.dart';
+
+class LocationController extends GetxController implements GetxService {
+  final LocationRepo locationRepo;
+
+  LocationController({required this.locationRepo});
+
+  bool isLoading = false;
+  late Position _position;
+  late Position _pickPosition;
+
+  Placemark _placemark = Placemark();
+  Placemark _pickPlacemark = Placemark();
+
+  List<AddressModel> _addressList = [];
+  late List<AddressModel> _allAddressList = [];
+
+  List<String> _addressTypeList = ['home', 'office', 'other'];
+  int _addressTypeIndex = 0;
 }
