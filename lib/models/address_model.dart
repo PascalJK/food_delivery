52eb535dd3c8c3f169815de2c3917e9f64110be4
Pascal JK <pascual.santos80@gmail.com>
2023-09-27 22:56:13 +0200

    - working on model properties

--- lib/models/address_model.dart
+++ lib/models/address_model.dart
@@ -1,2 +1,19 @@
 class AddressModel {
+  final int? id;
+  final String type;
+  final String? contactPersonName;
+  final String? contactPersonNumber;
+  final String address;
+  final String latitude;
+  final String longitude;
+
+  AddressModel({
+    this.id,
+    required this.type,
+    this.contactPersonName,
+    this.contactPersonNumber,
+    required this.address,
+    required this.latitude,
+    required this.longitude,
+  });
 }
