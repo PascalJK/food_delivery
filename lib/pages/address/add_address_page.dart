8a9787dad06c8c655d5f74be9f956384cafca8d1
Pascal JK <pascual.santos80@gmail.com>
2023-09-27 23:09:28 +0200

    - added class.

---
+++ lib/pages/address/add_address_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddAdressPage extends StatefulWidget {
+  const AddAdressPage({super.key});
+
+  @override
+  State<AddAdressPage> createState() => _AddAdressPageState();
+}
+
+class _AddAdressPageState extends State<AddAdressPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
