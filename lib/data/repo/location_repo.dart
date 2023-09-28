ed7c098fb2eb588985246c4a878ee4e4e3618d5e
Pascal JK <pascual.santos80@gmail.com>
2023-09-28 14:30:27 +0200

    - working on location repo file.

--- lib/data/repo/location_repo.dart
+++ lib/data/repo/location_repo.dart
@@ -1,4 +1,7 @@
 import 'package:food_delivery/data/api/api_client.dart';
+import 'package:food_delivery/utils/app_constants.dart';
+import 'package:get/get.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class LocationRepo {
@@ -6,4 +9,9 @@ class LocationRepo {
   final SharedPreferences sharedPreferences;
 
   LocationRepo({required this.apiClient, required this.sharedPreferences});
+
+  Future<Response> getAddressfromGeocode(LatLng latLng) async {
+    return await apiClient
+        .getData('${AppConstants.GEOCODE_URI}?lat=${latLng.latitude}&lng=${latLng.longitude}');
+  }
 }
