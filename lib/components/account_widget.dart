82243df879ae4fc39692d6277e51488af2d75e62
Pascal JK <pascual.santos80@gmail.com>
2023-09-28 12:36:17 +0200

    - working on account widget.

--- lib/components/account_widget.dart
+++ lib/components/account_widget.dart
@@ -10,41 +10,43 @@ class AccountWidget extends StatelessWidget {
     required this.iconData,
     required this.bgColor,
     required this.text,
+    this.dividerHeight,
+    this.onPressed,
   });
   final IconData iconData;
   final Color bgColor;
   final String text;
+  final double? dividerHeight;
+  final void Function()? onPressed;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.maxFinite,
-      padding: EdgeInsets.fromLTRB(
-        Dimensions.width20,
-        Dimensions.width10,
-        0,
-        Dimensions.width10,
-      ),
-      decoration: BoxDecoration(color: Colors.white, boxShadow: [
-        BoxShadow(
-          blurRadius: 1,
-          offset: const Offset(0, 2),
-          color: Colors.grey.withOpacity(.2),
-        ),
-      ]),
-      child: Row(
-        children: [
-          AppIcon(
-            icon: iconData,
-            bgColor: bgColor,
-            iconColor: Colors.white,
-            iconSize: Dimensions.iconSize24,
-            size: Dimensions.iconSize24 * 2,
+    return Column(
+      children: [
+        MaterialButton(
+          onPressed: onPressed,
+          padding: EdgeInsets.fromLTRB(
+            Dimensions.width20,
+            Dimensions.width10,
+            0,
+            Dimensions.width10,
           ),
-          SizedBox(width: Dimensions.width10),
-          BigText(text: text)
-        ],
-      ),
+          child: Row(
+            children: [
+              AppIcon(
+                icon: iconData,
+                bgColor: bgColor,
+                iconColor: Colors.white,
+                iconSize: Dimensions.iconSize24,
+                size: Dimensions.iconSize24 * 2,
+              ),
+              SizedBox(width: Dimensions.width10),
+              BigText(text: text)
+            ],
+          ),
+        ),
+        Divider(thickness: dividerHeight ?? 1),
+      ],
     );
   }
 }
