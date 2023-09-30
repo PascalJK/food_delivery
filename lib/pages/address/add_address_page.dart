42e1a83a052abe791d8e86982e9fc9f25d69f967
Pascal JK <pascual.santos80@gmail.com>
2023-09-30 09:36:25 +0200

    - wrapped column inside scrollview.

--- lib/pages/address/add_address_page.dart
+++ lib/pages/address/add_address_page.dart
@@ -47,114 +47,116 @@ class _AddAddressPageState extends State<AddAddressPage> {
       body: GetBuilder<LocationController>(
           init: controller.loadAccountData(),
           builder: (c) {
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                // map view
-                Container(
-                  height: 140,
-                  width: MediaQuery.of(context).size.width,
-                  margin: const EdgeInsets.fromLTRB(5, 5, 5, 0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(5),
-                    border: Border.all(
-                      width: 2,
-                      color: AppColors.mainColor,
+            return SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // map view
+                  Container(
+                    height: 140,
+                    width: MediaQuery.of(context).size.width,
+                    margin: const EdgeInsets.fromLTRB(5, 5, 5, 0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                      border: Border.all(
+                        width: 2,
+                        color: AppColors.mainColor,
+                      ),
+                    ),
+                    child: Stack(
+                      children: [
+                        GoogleMap(
+                          initialCameraPosition: CameraPosition(target: _initPos, zoom: 17),
+                          compassEnabled: false,
+                          mapToolbarEnabled: false,
+                          indoorViewEnabled: true,
+                          zoomControlsEnabled: false,
+                          onCameraIdle: () => c.updateCameraPosition(_camPos, true),
+                          onCameraMove: (position) => _camPos = position,
+                          onMapCreated: (mapController) {
+                            c.setMapController(mapController);
+                          },
+                        )
+                      ],
                     ),
                   ),
-                  child: Stack(
-                    children: [
-                      GoogleMap(
-                        initialCameraPosition: CameraPosition(target: _initPos, zoom: 17),
-                        compassEnabled: false,
-                        mapToolbarEnabled: false,
-                        indoorViewEnabled: true,
-                        zoomControlsEnabled: false,
-                        onCameraIdle: () => c.updateCameraPosition(_camPos, true),
-                        onCameraMove: (position) => _camPos = position,
-                        onMapCreated: (mapController) {
-                          c.setMapController(mapController);
+                  // Text Views
+                  Padding(
+                    padding: EdgeInsets.only(left: Dimensions.width20, top: Dimensions.height20),
+                    child: SizedBox(
+                      height: 50,
+                      child: ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: c.addressTypeList.length,
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (context, index) {
+                          return InkWell(
+                            onTap: () => controller.setAddressTypeIndex(index),
+                            child: Container(
+                              padding: EdgeInsets.symmetric(
+                                horizontal: Dimensions.width20,
+                                vertical: Dimensions.height10,
+                              ),
+                              margin: const EdgeInsets.symmetric(horizontal: 5, vertical: 3),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(Dimensions.radius5),
+                                color: context.theme.cardColor,
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.grey[200]!,
+                                    spreadRadius: 1,
+                                    blurRadius: 5,
+                                  ),
+                                ],
+                              ),
+                              child: Icon(
+                                index == 0
+                                    ? Icons.home_filled
+                                    : index == 1
+                                        ? Icons.work
+                                        : Icons.location_on,
+                                color: c.addressTypeIndex == index
+                                    ? AppColors.mainColor
+                                    : context.theme.disabledColor,
+                              ),
+                            ),
+                          );
                         },
-                      )
-                    ],
-                  ),
-                ),
-                // Text Views
-                Padding(
-                  padding: EdgeInsets.only(left: Dimensions.width20, top: Dimensions.height20),
-                  child: SizedBox(
-                    height: 50,
-                    child: ListView.builder(
-                      shrinkWrap: true,
-                      itemCount: c.addressTypeList.length,
-                      scrollDirection: Axis.horizontal,
-                      itemBuilder: (context, index) {
-                        return InkWell(
-                          onTap: () => controller.setAddressTypeIndex(index),
-                          child: Container(
-                            padding: EdgeInsets.symmetric(
-                              horizontal: Dimensions.width20,
-                              vertical: Dimensions.height10,
-                            ),
-                            margin: const EdgeInsets.symmetric(horizontal: 5, vertical: 3),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(Dimensions.radius5),
-                              color: context.theme.cardColor,
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.grey[200]!,
-                                  spreadRadius: 1,
-                                  blurRadius: 5,
-                                ),
-                              ],
-                            ),
-                            child: Icon(
-                              index == 0
-                                  ? Icons.home_filled
-                                  : index == 1
-                                      ? Icons.work
-                                      : Icons.location_on,
-                              color: c.addressTypeIndex == index
-                                  ? AppColors.mainColor
-                                  : context.theme.disabledColor,
-                            ),
-                          ),
-                        );
-                      },
+                      ),
                     ),
                   ),
-                ),
-                SizedBox(height: Dimensions.height20),
-                Padding(
-                  padding: EdgeInsets.only(left: Dimensions.width20, bottom: Dimensions.height10),
-                  child: const BigText(text: 'Delivery Address'),
-                ),
-                TextInputField(
-                  controller: c.addressController,
-                  icon: Icons.map_outlined,
-                  hint: 'Your Address',
-                ),
-                Padding(
-                  padding: EdgeInsets.only(left: Dimensions.width20, top: Dimensions.height10),
-                  child: const BigText(text: 'Your Name'),
-                ),
-                SizedBox(height: Dimensions.height15),
-                TextInputField(
-                  controller: c.cPersonName,
-                  icon: Icons.person,
-                  hint: 'Your Name',
-                ),
-                Padding(
-                  padding: EdgeInsets.only(left: Dimensions.width20, top: Dimensions.height10),
-                  child: const BigText(text: 'Your Number'),
-                ),
-                SizedBox(height: Dimensions.height15),
-                TextInputField(
-                  controller: c.cPersonNumber,
-                  icon: Icons.phone,
-                  hint: 'Your Contact',
-                ),
-              ],
+                  SizedBox(height: Dimensions.height20),
+                  Padding(
+                    padding: EdgeInsets.only(left: Dimensions.width20, bottom: Dimensions.height10),
+                    child: const BigText(text: 'Delivery Address'),
+                  ),
+                  TextInputField(
+                    controller: c.addressController,
+                    icon: Icons.map_outlined,
+                    hint: 'Your Address',
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: Dimensions.width20, top: Dimensions.height10),
+                    child: const BigText(text: 'Your Name'),
+                  ),
+                  SizedBox(height: Dimensions.height15),
+                  TextInputField(
+                    controller: c.cPersonName,
+                    icon: Icons.person,
+                    hint: 'Your Name',
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: Dimensions.width20, top: Dimensions.height10),
+                    child: const BigText(text: 'Your Number'),
+                  ),
+                  SizedBox(height: Dimensions.height15),
+                  TextInputField(
+                    controller: c.cPersonNumber,
+                    icon: Icons.phone,
+                    hint: 'Your Contact',
+                  ),
+                ],
+              ),
             );
           }),
     );
