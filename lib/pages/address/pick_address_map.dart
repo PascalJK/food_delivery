a0e39fbd43bc009a989eb18b47d2f3f63ecaea1f
Pascal JK <pascual.santos80@gmail.com>
2023-10-02 11:05:12 +0200

    - working on pick address page.

--- lib/pages/address/pick_address_map.dart
+++ lib/pages/address/pick_address_map.dart
@@ -1,10 +1,91 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/controllers/location_controller.dart';
+import 'package:food_delivery/utils/colors.dart';
+import 'package:food_delivery/utils/dimensions.dart';
+import 'package:get/get.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class PickAddressMap extends StatelessWidget {
-  const PickAddressMap({super.key});
+  final GoogleMapController googleMapController;
+  PickAddressMap({super.key, required this.googleMapController});
+  var _camPos = const CameraPosition(target: LatLng(45.51563, -122.677433), zoom: 17);
+  late LatLng _initPos = const LatLng(45.51563, -122.677433);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return GetBuilder<LocationController>(initState: (state) {
+      var lc = Get.find<LocationController>();
+      // Is checking if user is logged in really needed??
+      // if user model is null first get user info...
+      // if (uc.getAccountModel.isUndefinedOrNull) {}
+
+      if (lc.addressList.isNotEmpty) {
+        _camPos = CameraPosition(
+            target: LatLng(
+          double.parse(lc.getAdress['latitude']),
+          double.parse(lc.getAdress['longitude']),
+        ));
+        _initPos = _camPos.target;
+      }
+    }, builder: (c) {
+      return Scaffold(
+        body: Stack(
+          children: [
+            GoogleMap(
+              initialCameraPosition: CameraPosition(target: _initPos, zoom: 17),
+              compassEnabled: false,
+              mapToolbarEnabled: false,
+              indoorViewEnabled: true,
+              zoomControlsEnabled: false,
+              myLocationEnabled: true,
+              onCameraIdle: () => c.updateCameraPosition(_camPos, false),
+              onCameraMove: (position) => _camPos = position,
+              onMapCreated: (mapController) {
+                c.setMapController(mapController);
+              },
+            ),
+            Center(
+              child: !c.isloading
+                  ? Image.asset('assets/image/pick_marker.png', height: 50, width: 50)
+                  : const CircularProgressIndicator(),
+            ),
+            Positioned(
+              top: Dimensions.height45,
+              left: Dimensions.width20,
+              right: Dimensions.width20,
+              child: Container(
+                height: 50,
+                padding: EdgeInsets.symmetric(horizontal: Dimensions.width10),
+                decoration: BoxDecoration(
+                  color: AppColors.mainColor,
+                  borderRadius: BorderRadius.circular(Dimensions.radius20 / 2),
+                ),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.location_on,
+                      size: Dimensions.iconSize24,
+                      color: AppColors.yellowColor,
+                    ),
+                    const SizedBox(width: 3),
+                    Expanded(
+                      child: Text(
+                        c.pickPlaceMark.name ?? '',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: Dimensions.font17,
+                        ),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    });
   }
 }
