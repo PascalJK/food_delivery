7e45c3a279dda23616f822a2d5b1bd2c8080a2f5
Pascal JK <82771457+PascalJK@users.noreply.github.com>
2024-06-20 22:17:32 +0200

    - updated home page ui + package import.

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -4,7 +4,7 @@ import 'package:food_delivery/pages/account/account_page.dart';
 import 'package:food_delivery/pages/cart/cart_history_page.dart';
 import 'package:food_delivery/pages/home/main_food_page.dart';
 import 'package:food_delivery/utils/colors.dart';
-import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
+import 'package:persistent_bottom_nav_bar_v2/persistent_bottom_nav_bar_v2.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -16,68 +16,70 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final _controller = PersistentTabController(initialIndex: 0);
 
-  final List<Widget> _pages = [
-    const MainFoodPage(),
-    const Center(child: Text('Test Page_1')),
-    const CartHistoryPage(),
-    const AccountPage(),
-  ];
-
-  final List<PersistentBottomNavBarItem> _barItems = [
-    PersistentBottomNavBarItem(
-      icon: const Icon(Icons.home_outlined),
-      title: 'Home',
-      activeColorPrimary: AppColors.mainColor,
-      inactiveColorPrimary: CupertinoColors.systemGrey,
+  final List<PersistentTabConfig> _tabs = [
+    PersistentTabConfig(
+      screen: const MainFoodPage(),
+      item: ItemConfig(
+        icon: const Icon(Icons.home_outlined),
+        title: "Home",
+        activeForegroundColor: AppColors.mainColor,
+        inactiveBackgroundColor: CupertinoColors.systemGrey,
+      ),
     ),
-    PersistentBottomNavBarItem(
-      icon: const Icon(Icons.history),
-      title: 'History',
-      activeColorPrimary: AppColors.mainColor,
-      inactiveColorPrimary: CupertinoColors.systemGrey,
+    PersistentTabConfig(
+      screen: const Center(child: Text('Test Page_1')),
+      item: ItemConfig(
+        icon: const Icon(Icons.history),
+        title: 'History',
+        activeForegroundColor: AppColors.mainColor,
+        inactiveBackgroundColor: CupertinoColors.systemGrey,
+      ),
     ),
-    PersistentBottomNavBarItem(
-      icon: const Icon(Icons.shopping_cart),
-      title: 'Cart',
-      activeColorPrimary: AppColors.mainColor,
-      inactiveColorPrimary: CupertinoColors.systemGrey,
+    PersistentTabConfig(
+      screen: const CartHistoryPage(),
+      item: ItemConfig(
+        icon: const Icon(Icons.shopping_cart),
+        title: 'Cart',
+        activeForegroundColor: AppColors.mainColor,
+        inactiveBackgroundColor: CupertinoColors.systemGrey,
+      ),
     ),
-    PersistentBottomNavBarItem(
-      icon: const Icon(Icons.person),
-      title: 'Account',
-      activeColorPrimary: AppColors.mainColor,
-      inactiveColorPrimary: CupertinoColors.systemGrey,
+    PersistentTabConfig(
+      screen: const AccountPage(),
+      item: ItemConfig(
+        icon: const Icon(Icons.person),
+        title: 'Account',
+        activeForegroundColor: AppColors.mainColor,
+        inactiveBackgroundColor: CupertinoColors.systemGrey,
+      ),
     ),
   ];
 
   @override
   Widget build(BuildContext context) {
     return PersistentTabView(
-      context,
       controller: _controller,
-      screens: _pages,
-      items: _barItems,
-      confineInSafeArea: true,
-      resizeToAvoidBottomInset: true,
-      hideNavigationBarWhenKeyboardShows: true,
-      decoration: const NavBarDecoration(
-        borderRadius: BorderRadius.vertical(top: Radius.circular(10)),
-        colorBehindNavBar: Colors.white,
+      tabs: _tabs,
+      navBarBuilder: (navBarConfig) => Style6BottomNavBar(
+        navBarConfig: navBarConfig,
       ),
+      resizeToAvoidBottomInset: true,
+      // hideNavigationBarWhenKeyboardShows: true,
+      // decoration: const NavBarDecoration(
+      //   borderRadius: BorderRadius.vertical(top: Radius.circular(10)),
+      //   colorBehindNavBar: Colors.white,
+      // ),
       popAllScreensOnTapOfSelectedTab: true,
       popActionScreens: PopActionScreensType.all,
-      itemAnimationProperties: const ItemAnimationProperties(
-        // Navigation Bar's items animation properties.
-        duration: Duration(milliseconds: 200),
-        curve: Curves.ease,
-      ),
+      // itemAnimationProperties: const ItemAnimationProperties(
+      //   // Navigation Bar's items animation properties.
+      //   duration: Duration(milliseconds: 200),
+      //   curve: Curves.ease,
+      // ),
       screenTransitionAnimation: const ScreenTransitionAnimation(
-        // Screen transition animation on change of selected tab.
-        animateTabTransition: true,
         curve: Curves.ease,
         duration: Duration(milliseconds: 450),
       ),
-      navBarStyle: NavBarStyle.style6,
     );
   }
 }
